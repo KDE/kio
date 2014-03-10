54dd63249f54c668facc58183bf1c2b9da0f8c7a
Alex Richardson <arichardson.kde@gmail.com>
2014-03-10 19:20:46 +0100

    Fix check for HAVE_DIRENT_D_TYPE
    CMake uses /usr/bin/cc to compile the check_struct_has_member source file
    therefore we must use struct dirent and not just dirent

--- src/ioslaves/file/ConfigureChecks.cmake
+++ src/ioslaves/file/ConfigureChecks.cmake
@@ -12,4 +12,4 @@ check_function_exists(posix_fadvise    HAVE_FADVISE)                  # kioslave
 
 check_library_exists(volmgt volmgt_running "" HAVE_VOLMGT)
 
-check_struct_has_member(dirent d_type dirent.h HAVE_DIRENT_D_TYPE)
+check_struct_has_member("struct dirent" d_type dirent.h HAVE_DIRENT_D_TYPE LANGUAGE CXX)
