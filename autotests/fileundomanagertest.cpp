KDE/kio

commit 211861c8223fee5cc420f96be6769ddbfb994da9

Fix tests in kio
REVIEW: 118104

autotests/fileundomanagertest.cpp
@@ -59,7 +59,7 @@ using namespace KIO;
 
 static QString homeTmpDir()
 {
-    return QStandardPaths::writableLocation(QStandardPaths::DataLocation) + '/';
+    return QStandardPaths::writableLocation(QStandardPaths::DataLocation);
 }
 static QString destDir()
 {

src/core/kprotocolinfofactory.cpp
@@ -44,7 +44,7 @@ KProtocolInfoFactory::~KProtocolInfoFactory()
 static QStringList servicesDirs()
 {
     return QStandardPaths::locateAll(QStandardPaths::GenericDataLocation,
-                                     QLatin1String("kde5/services"),
+                                     QLatin1String("kservices5"),
                                      QStandardPaths::LocateDirectory);
 }
 

commit e0b806984b027e6f69b2cab8c74f5417359e3caa

211861c8223fee5cc420f96be6769ddbfb994da9 broke fileundomanagertest. This fixes it by appending a slash using QDir::separator()
Reviewed by: Rohan Garg

autotests/fileundomanagertest.cpp
@@ -59,7 +59,7 @@ using namespace KIO;
 
 static QString homeTmpDir()
 {
-    return QStandardPaths::writableLocation(QStandardPaths::DataLocation);
+    return QStandardPaths::writableLocation(QStandardPaths::DataLocation) + QDir::separator();
 }
 static QString destDir()
 {
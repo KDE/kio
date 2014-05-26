e0b806984b027e6f69b2cab8c74f5417359e3caa
Mark Gaiser <markg85@gmail.com>
2014-05-26 12:50:22 +0200

    211861c8223fee5cc420f96be6769ddbfb994da9 broke fileundomanagertest. This fixes it by appending a slash using QDir::separator()
    Reviewed by: Rohan Garg

--- autotests/fileundomanagertest.cpp
+++ autotests/fileundomanagertest.cpp
@@ -59,7 +59,7 @@ using namespace KIO;
 
 static QString homeTmpDir()
 {
-    return QStandardPaths::writableLocation(QStandardPaths::DataLocation);
+    return QStandardPaths::writableLocation(QStandardPaths::DataLocation) + QDir::separator();
 }
 static QString destDir()
 {
