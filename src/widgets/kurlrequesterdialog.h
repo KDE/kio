fcd52b72ce9e0cec6a07d1f5a79baf903b7ee468
David Faure <faure@kde.org>
2014-08-24 23:31:43 +0200

    slightly better docu

--- src/widgets/kurlrequesterdialog.h
+++ src/widgets/kurlrequesterdialog.h
@@ -83,7 +83,7 @@ public:
 
     /**
      * Returns a pointer to the file dialog used by the KUrlRequester.
-     * @deprecated since 5.0
+     * @deprecated since 5.0, use urlRequester() methods instead.
      */
 #ifndef KIOWIDGETS_NO_DEPRECATED
     KIOWIDGETS_DEPRECATED QFileDialog *fileDialog();
