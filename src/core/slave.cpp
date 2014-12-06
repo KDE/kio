51caa053b07c8c0730e81c150252f26f36eb2093
Jan Kundrát <jkt@kde.org>
2014-12-06 17:31:42 +0100

    Fix check for missing `kioslave` executable
    The old version checked a string which is guaranteed to never be empty
    for being empty. It's better to check whether that file is actually an
    executable file.
    
    Change-Id: If4c0417a5f514b4232a0b3a57ad40d49724f9cfe

--- src/core/slave.cpp
+++ src/core/slave.cpp
@@ -479,8 +479,9 @@ Slave *Slave::createSlave(const QString &protocol, const QUrl &url, int &error, 
         //qDebug() << "kioslave" << ", " << lib_path << ", " << protocol << ", " << QString() << ", " << slaveAddress;
 
         const QString kioslave = CMAKE_INSTALL_FULL_LIBEXECDIR_KF5 "/kioslave";
-        if (kioslave.isEmpty()) {
-            error_text = i18n("Can not find 'kioslave' executable");
+        QFileInfo kioslaveInfo(kioslave);
+        if (!kioslaveInfo.isFile() || !kioslaveInfo.isExecutable()) {
+            error_text = i18n("Can not find 'kioslave' executable at '%1'", QLatin1String(CMAKE_INSTALL_FULL_LIBEXECDIR_KF5));
             error = KIO::ERR_CANNOT_LAUNCH_PROCESS;
             delete slave;
             return 0;
