737e7e50a0e0b0eb66e773ec785bc7e758094d50
David Faure <faure@kde.org>
2015-01-14 18:47:26 +0100

    KIO: new class KFileCopyToMenu. Adds Copy To / Move To to popupmenus.
    Mostly for filemanager-like applications, which is why it's going into
    KIOFileWidgets.
    
    Moved from libkonq, added error signal for dolphin to avoid messageboxes, added unittest.
    
    CHANGELOG: new class KFileCopyToMenu. Adds "Copy To / Move To" to popupmenus.
    
    Change-Id: I64baa301016bdd3a31b7590c10d1218d76a7cc3a

---
+++ src/filewidgets/kfilecopytomenu.h
@@ -0,0 +1,89 @@
+/* Copyright 2008, 2015 David Faure <faure@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KFILECOPYTOMENU_H
+#define KFILECOPYTOMENU_H
+
+#include <QUrl>
+#include <QObject>
+
+#include <kiofilewidgets_export.h>
+
+class QMenu;
+class KFileCopyToMenuPrivate;
+
+/**
+ * This class adds "Copy To" and "Move To" submenus to a popupmenu.
+ */
+class KIOFILEWIDGETS_EXPORT KFileCopyToMenu : public QObject
+{
+    Q_OBJECT
+public:
+    /**
+     * Creates a KFileCopyToMenu instance
+     * Note that this instance (and the widget) must stay alive for at least as
+     * long as the popupmenu; it has the slots for the actions created by addActionsTo.
+     *
+     * @param parentWidget parent widget for the file dialog and message boxes.
+     * The parentWidget also serves as a parent for this object.
+     */
+    KFileCopyToMenu(QWidget *parentWidget);
+
+    /**
+     * Destructor
+     */
+    ~KFileCopyToMenu();
+
+    /**
+     * Sets the URLs which the actions apply to.
+     */
+    void setUrls(const QList<QUrl> &urls);
+
+    /**
+     * If setReadOnly(true) is called, the "Move To" submenu will not appear.
+     */
+    void setReadOnly(bool ro);
+
+    /**
+     * Generate the actions and submenus, and adds them to the @p menu.
+     * All actions are created as children of the menu.
+     */
+    void addActionsTo(QMenu *menu) const;
+
+    /**
+     * Enables or disables automatic error handling with message boxes.
+     * When called with true, a messagebox is shown in case of an error during a copy or move.
+     * When called with false, the application should connect to the error signal instead.
+     * Auto error handling is disabled by default.
+     */
+    void setAutoErrorHandlingEnabled(bool b);
+
+Q_SIGNALS:
+    /**
+     * Emitted when the copy or move job fails.
+     * @param errorCode the KIO job error code
+     * @param message the error message to show the user
+     */
+    void error(int errorCode, const QString &message);
+
+private:
+    KFileCopyToMenuPrivate *const d;
+};
+
+#endif
---
+++ src/filewidgets/kfilecopytomenu_p.h
@@ -0,0 +1,85 @@
+/* Copyright 2008, 2015 David Faure <faure@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include <kconfiggroup.h>
+#include <QMenu>
+#include <QActionGroup>
+#include <QObject>
+#include <QUrl>
+
+class KFileCopyToMenuPrivate
+{
+public:
+    KFileCopyToMenuPrivate(KFileCopyToMenu *qq, QWidget *parentWidget);
+
+    KFileCopyToMenu *q;
+    QList<QUrl> m_urls;
+    QWidget *m_parentWidget;
+    bool m_readOnly;
+    bool m_autoErrorHandling;
+};
+
+enum MenuType { Copy, Move };
+
+// The main menu, shown when opening "Copy To" or "Move To"
+// It contains Home Folder, Root Folder, Browse, and recent destinations
+class KFileCopyToMainMenu : public QMenu
+{
+    Q_OBJECT
+public:
+    KFileCopyToMainMenu(QMenu *parent, KFileCopyToMenuPrivate *d, MenuType menuType);
+
+    QActionGroup &actionGroup()
+    {
+        return m_actionGroup;    // used by submenus
+    }
+    MenuType menuType() const
+    {
+        return m_menuType;    // used by submenus
+    }
+
+private Q_SLOTS:
+    void slotAboutToShow();
+    void slotBrowse();
+    void slotTriggered(QAction *action);
+
+private:
+    void copyOrMoveTo(const QUrl &dest);
+
+private:
+    MenuType m_menuType;
+    QActionGroup m_actionGroup;
+    KFileCopyToMenuPrivate *d; // this isn't our own d pointer, it's the one for the public class
+    KConfigGroup m_recentDirsGroup;
+};
+
+// The menu that lists a directory
+class KFileCopyToDirectoryMenu : public QMenu
+{
+    Q_OBJECT
+public:
+    KFileCopyToDirectoryMenu(QMenu *parent, KFileCopyToMainMenu *mainMenu, const QString &path);
+
+private Q_SLOTS:
+    void slotAboutToShow();
+
+private:
+    KFileCopyToMainMenu *m_mainMenu;
+    QString m_path;
+};
