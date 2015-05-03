36400c2f6e038e5028bf6347108270ec50cb1de2
Boris Egorov <egorov@linux.com>
2015-05-03 16:44:26 +0600

    kio: Fix conditions with QFile::rename return value
    See my previous commit: e5c3f7eda489.
    
    I grepped kf5 sources to search for more issues with QFile::rename
    return value. This patch fixes remaining ones.
    
    REVIEW: 123598
    CHANGELOG: kio: Fix conditions with QFile::rename return value

--- src/core/copyjob.cpp
+++ src/core/copyjob.cpp
@@ -1823,15 +1823,15 @@ void CopyJobPrivate::slotResultRenaming(KJob *job)
                     tmpFile.close();
                     tmpFile.remove();
                     //qDebug() << "QTemporaryFile using" << _tmp << "as intermediary";
-                    if (QFile::rename(_src, _tmp) == 0) {
+                    if (QFile::rename(_src, _tmp)) {
                         //qDebug() << "Renaming" << _src << "to" << _tmp << "succeeded";
-                        if (!QFile::exists(_dest) && QFile::rename(_tmp, _dest) == 0) {
+                        if (!QFile::exists(_dest) && QFile::rename(_tmp, _dest)) {
                             err = 0;
                             org::kde::KDirNotify::emitFileRenamed(m_currentSrcURL, dest);
                         } else {
                             //qDebug() << "Didn't manage to rename" << _tmp << "to" << _dest << ", reverting";
                             // Revert back to original name!
-                            if (QFile::rename(_tmp, _src) != 0) {
+                            if (!QFile::rename(_tmp, _src)) {
                                 qWarning() << "Couldn't rename" << _tmp << "back to" << _src << '!';
                                 // Severe error, abort
                                 q->Job::slotResult(job); // will set the error and emit result(this)
