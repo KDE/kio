KDE/kio

commit 46cd152fb057bcd1f4391ff52b3eee8e6f5379bf

Return inode/directory when isDir returns true (kfileitem)
Also I have added tests to make sure that this change causes no
regresion (as asked in the review).

REVIEW: 121447

autotests/kfileitemtest.cpp
@@ -422,6 +422,55 @@ void KFileItemTest::testIconNameForUrl()
     QCOMPARE(KIO::iconNameForUrl(QUrl(url)), expectedIcon);
 }
 
+void KFileItemTest::testMimetypeForRemoteFolder()
+{
+    KIO::UDSEntry entry;
+    entry.insert(KIO::UDSEntry::UDS_NAME, "foo");
+    entry.insert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
+    QUrl url("smb://remoteFolder/foo");
+    KFileItem fileItem(entry, url);
+
+    QCOMPARE(fileItem.mimetype(), QStringLiteral("inode/directory"));
+}
+
+void KFileItemTest::testMimetypeForRemoteFolderWithFileType()
+{
+    QString udsMimeType = QStringLiteral("application/x-smb-workgroup");
+    KIO::UDSEntry entry;
+    entry.insert(KIO::UDSEntry::UDS_NAME, "foo");
+    entry.insert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
+    entry.insert(KIO::UDSEntry::UDS_MIME_TYPE, udsMimeType);
+
+    QUrl url("smb://remoteFolder/foo");
+    KFileItem fileItem(entry, url);
+
+    QCOMPARE(fileItem.mimetype(), udsMimeType);
+}
+
+void KFileItemTest::testCurrentMimetypeForRemoteFolder()
+{
+    KIO::UDSEntry entry;
+    entry.insert(KIO::UDSEntry::UDS_NAME, "foo");
+    entry.insert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
+    QUrl url("smb://remoteFolder/foo");
+    KFileItem fileItem(entry, url);
+
+    QCOMPARE(fileItem.currentMimeType().name(), QStringLiteral("inode/directory"));
+}
+
+void KFileItemTest::testCurrentMimetypeForRemoteFolderWithFileType()
+{
+    QString udsMimeType = QStringLiteral("application/x-smb-workgroup");
+    KIO::UDSEntry entry;
+    entry.insert(KIO::UDSEntry::UDS_NAME, "foo");
+    entry.insert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
+    entry.insert(KIO::UDSEntry::UDS_MIME_TYPE, udsMimeType);
+
+    QUrl url("smb://remoteFolder/foo");
+    KFileItem fileItem(entry, url);
+
+    QCOMPARE(fileItem.currentMimeType().name(), udsMimeType);
+}
 
 #ifndef Q_OS_WIN // user/group/other write permissions are not handled on windows
 

autotests/kfileitemtest.h
@@ -37,6 +37,11 @@ private Q_SLOTS:
     void testCmp();
     void testRename();
     void testDotDirectory();
+    void testMimetypeForRemoteFolder();
+    void testMimetypeForRemoteFolderWithFileType();
+    void testCurrentMimetypeForRemoteFolder();
+    void testCurrentMimetypeForRemoteFolderWithFileType();
+
 #ifndef Q_OS_WIN
     void testIsReadable_data();
     void testIsReadable();

src/core/kfileitem.cpp
@@ -730,6 +730,10 @@ QMimeType KFileItem::determineMimeType() const
 
     if (!d->m_mimeType.isValid() || !d->m_bMimeTypeKnown) {
         QMimeDatabase db;
+        if (isDir()) {
+            d->m_mimeType = db.mimeTypeForName("inode/directory");
+            return d->m_mimeType;
+        }
         bool isLocalUrl;
         const QUrl url = mostLocalUrl(&isLocalUrl);
         d->m_mimeType = db.mimeTypeForUrl(url);
@@ -1453,6 +1457,10 @@ QMimeType KFileItem::currentMimeType() const
         // On-demand fast (but not always accurate) mimetype determination
         Q_ASSERT(!d->m_url.isEmpty());
         QMimeDatabase db;
+        if (isDir()) {
+            d->m_mimeType = db.mimeTypeForName("inode/directory");
+            return d->m_mimeType;
+        }
         const QUrl url = mostLocalUrl();
         if (d->m_delayedMimeTypes) {
             const QList<QMimeType> mimeTypes = db.mimeTypesForFileName(url.path());

commit d8aafbc37daab3cd62ee77430a55b3d72ccc12e0

Show custom folder icons again
Set mimetype as known when we assign the mime type for inode/directory

This bug was introduced by commit 46cd152f

BUG: 350612
FIXED-IN: 5.14
REVIEW: 124690

autotests/kfileitemtest.cpp
@@ -25,6 +25,8 @@
 #include <qtemporarydir.h>
 #include <qtemporaryfile.h>
 #include <kuser.h>
+#include <kdesktopfile.h>
+#include <kconfiggroup.h>
 
 QTEST_MAIN(KFileItemTest)
 
@@ -470,6 +472,25 @@ void KFileItemTest::testCurrentMimetypeForRemoteFolderWithFileType()
     KFileItem fileItem(entry, url);
 
     QCOMPARE(fileItem.currentMimeType().name(), udsMimeType);
+}
+
+void KFileItemTest::testIconNameForCustomFolderIcons()
+{
+    // Custom folder icons should be displayed (bug 350612)
+
+    const QString iconName = QStringLiteral("folder-music");
+
+    QTemporaryDir tempDir;
+    const QUrl url = QUrl::fromLocalFile(tempDir.path());
+    KDesktopFile cfg(tempDir.path() + QString::fromLatin1("/.directory"));
+    cfg.desktopGroup().writeEntry("Icon", iconName);
+    cfg.sync();
+
+    KIO::UDSEntry entry;
+    entry.insert(KIO::UDSEntry::UDS_FILE_TYPE, S_IFDIR);
+    KFileItem fileItem(entry, url);
+
+    QCOMPARE(fileItem.iconName(), iconName);
 }
 
 #ifndef Q_OS_WIN // user/group/other write permissions are not handled on windows

autotests/kfileitemtest.h
@@ -41,6 +41,7 @@ private Q_SLOTS:
     void testMimetypeForRemoteFolderWithFileType();
     void testCurrentMimetypeForRemoteFolder();
     void testCurrentMimetypeForRemoteFolderWithFileType();
+    void testIconNameForCustomFolderIcons();
 
 #ifndef Q_OS_WIN
     void testIsReadable_data();

src/core/kfileitem.cpp
@@ -732,15 +732,15 @@ QMimeType KFileItem::determineMimeType() const
         QMimeDatabase db;
         if (isDir()) {
             d->m_mimeType = db.mimeTypeForName("inode/directory");
-            return d->m_mimeType;
+        } else {
+            bool isLocalUrl;
+            const QUrl url = mostLocalUrl(&isLocalUrl);
+            d->m_mimeType = db.mimeTypeForUrl(url);
+            // was:  d->m_mimeType = KMimeType::findByUrl( url, d->m_fileMode, isLocalUrl );
+            // => we are no longer using d->m_fileMode for remote URLs.
+            Q_ASSERT(d->m_mimeType.isValid());
+            //qDebug() << d << "finding final mimetype for" << url << ":" << d->m_mimeType.name();
         }
-        bool isLocalUrl;
-        const QUrl url = mostLocalUrl(&isLocalUrl);
-        d->m_mimeType = db.mimeTypeForUrl(url);
-        // was:  d->m_mimeType = KMimeType::findByUrl( url, d->m_fileMode, isLocalUrl );
-        // => we are no longer using d->m_fileMode for remote URLs.
-        Q_ASSERT(d->m_mimeType.isValid());
-        //qDebug() << d << "finding final mimetype for" << url << ":" << d->m_mimeType.name();
         d->m_bMimeTypeKnown = true;
     }
 