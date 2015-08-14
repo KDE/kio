KDE/kio

commit 4ce4fbe22fdb385f6c07acfd746b31362a4d5d8a

Use QVector instead of QSet as Kévin suggested to help with QSet ordering unstability.
Change-Id: Ibb57d32bc8930f321d4eed6c2189aeb53cc4643a
CCMAIL: ervin@kde.org

src/filewidgets/kfileplacesmodel.cpp
@@ -68,7 +68,7 @@ public:
     KFilePlacesModel *q;
 
     QList<KFilePlacesItem *> items;
-    QSet<QString> availableDevices;
+    QVector<QString> availableDevices;
     QMap<QObject *, QPersistentModelIndex> setupInProgress;
 
     Solid::Predicate predicate;
@@ -333,8 +333,7 @@ void KFilePlacesModel::Private::_k_deviceAdded(const QString &udi)
 
 void KFilePlacesModel::Private::_k_deviceRemoved(const QString &udi)
 {
-    if (availableDevices.contains(udi)) {
-        availableDevices.remove(udi);
+    if (availableDevices.removeOne(udi)) {
         _k_reloadBookmarks();
     }
 }
@@ -427,12 +426,12 @@ QList<KFilePlacesItem *> KFilePlacesModel::Private::loadBookmarkList()
 
     KBookmarkGroup root = bookmarkManager->root();
     KBookmark bookmark = root.first();
-    QSet<QString> devices = availableDevices;
+    QVector<QString> devices = availableDevices;
 
     while (!bookmark.isNull()) {
         QString udi = bookmark.metaDataItem("UDI");
         QString appName = bookmark.metaDataItem("OnlyInApp");
-        bool deviceAvailable = devices.remove(udi);
+        bool deviceAvailable = devices.removeOne(udi);
 
         bool allowedHere = appName.isEmpty() || (appName == QCoreApplication::instance()->applicationName());
 

commit 9b0a50f47abca61d0e9c40ba8072111e28c6304a

Fix build failure on Qt 5.3
This got introduced in commit 4ce4fbe22fdb385f6c07acfd746b31362a4d5d8a ;
QVector::removeOne is a 5.4 thing.

Change-Id: Ic45128e7ea1718b5a30d9fffacc7272592f93fba

src/filewidgets/kfileplacesmodel.cpp
@@ -333,7 +333,9 @@ void KFilePlacesModel::Private::_k_deviceAdded(const QString &udi)
 
 void KFilePlacesModel::Private::_k_deviceRemoved(const QString &udi)
 {
-    if (availableDevices.removeOne(udi)) {
+    auto it = std::find(availableDevices.begin(), availableDevices.end(), udi);
+    if (it != availableDevices.end()) {
+        availableDevices.erase(it);
         _k_reloadBookmarks();
     }
 }
@@ -431,7 +433,11 @@ QList<KFilePlacesItem *> KFilePlacesModel::Private::loadBookmarkList()
     while (!bookmark.isNull()) {
         QString udi = bookmark.metaDataItem("UDI");
         QString appName = bookmark.metaDataItem("OnlyInApp");
-        bool deviceAvailable = devices.removeOne(udi);
+        auto it = std::find(devices.begin(), devices.end(), udi);
+        bool deviceAvailable = (it != devices.end());
+        if (it != devices.end()) {
+            devices.erase(it);
+        }
 
         bool allowedHere = appName.isEmpty() || (appName == QCoreApplication::instance()->applicationName());
 