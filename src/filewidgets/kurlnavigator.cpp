KDE/kio

commit 9dbe36f734b5b839b2a6a934fad29d639e954498

Fix url encoding problems (e.g. when the path contains a '%') in KUrlNavigator
These changes also fix a weird behavior of KUrlNavigator when button 0 is
pressed. KUrlNavigator always used the local path '/' for button 0 and ignored
the scheme and authority.

BUG: 354678
REVIEW: 126009
FIXED-IN: 5.17

src/filewidgets/kurlnavigator.cpp
@@ -736,8 +736,8 @@ QUrl KUrlNavigator::Private::buttonUrl(int index) const
 
     // Keep scheme, hostname etc. as this is needed for e. g. browsing
     // FTP directories
-    const QUrl currentUrl = q->locationUrl();
-    QString pathOrUrl = currentUrl.toString(QUrl::PreferLocalFile);
+    QUrl url = q->locationUrl();
+    QString pathOrUrl = url.path();
 
     if (!pathOrUrl.isEmpty()) {
         if (index == 0) {
@@ -753,7 +753,8 @@ QUrl KUrlNavigator::Private::buttonUrl(int index) const
         }
     }
 
-    return QUrl::fromUserInput(pathOrUrl);
+    url.setPath(pathOrUrl);
+    return url;
 }
 
 void KUrlNavigator::Private::switchToBreadcrumbMode()

autotests/kurlnavigatortest.cpp
@@ -217,3 +217,40 @@ void KUrlNavigatorTest::testUrlParsing()
     QTest::keyClick(m_navigator->editor(), Qt::Key_Enter);
     QCOMPARE(m_navigator->locationUrl(), url);
 }
+
+void KUrlNavigatorTest::testButtonUrl_data()
+{
+    QTest::addColumn<QUrl>("locationUrl");
+    QTest::addColumn<int>("buttonIndex");
+    QTest::addColumn<QUrl>("expectedButtonUrl");
+
+    QTest::newRow("localPathButtonIndex3") << QUrl::fromLocalFile("/home/foo") << 3 << QUrl::fromLocalFile("/home/foo"); // out of range
+    QTest::newRow("localPathButtonIndex2") << QUrl::fromLocalFile("/home/foo") << 2 << QUrl::fromLocalFile("/home/foo");
+    QTest::newRow("localPathButtonIndex1") << QUrl::fromLocalFile("/home/foo") << 1 << QUrl::fromLocalFile("/home");
+    QTest::newRow("localPathButtonIndex0") << QUrl::fromLocalFile("/home/foo") << 0 << QUrl::fromLocalFile("/");
+
+    QTest::newRow("networkPathButtonIndex1") << QUrl::fromUserInput("network:/konqi.local/share") << 1 << QUrl::fromUserInput("network:/konqi.local");
+    QTest::newRow("networkPathButtonIndex0") << QUrl::fromUserInput("network:/konqi.local/share") << 0 << QUrl::fromUserInput("network:/");
+
+    QTest::newRow("ftpPathButtonIndex1") << QUrl::fromUserInput("ftp://kde.org/home/foo") << 1 << QUrl::fromUserInput("ftp://kde.org/home");
+    QTest::newRow("ftpPathButtonIndex0") << QUrl::fromUserInput("ftp://kde.org/home/foo") << 0 << QUrl::fromUserInput("ftp://kde.org/");
+
+    // bug 354678
+    QTest::newRow("localPathWithPercentage") << QUrl::fromLocalFile("/home/foo %/test") << 2 << QUrl::fromLocalFile("/home/foo %");
+}
+
+void KUrlNavigatorTest::testButtonUrl()
+{
+    QFETCH(QUrl, locationUrl);
+    QFETCH(int, buttonIndex);
+    QFETCH(QUrl, expectedButtonUrl);
+
+    // PREPARE
+    m_navigator->setLocationUrl(locationUrl);
+
+    // WHEN
+    const QUrl buttonUrl = m_navigator->url(buttonIndex);
+
+    // THEN
+    QCOMPARE(buttonUrl, expectedButtonUrl);
+}

autotests/kurlnavigatortest.h
@@ -41,6 +41,9 @@ private Q_SLOTS:
     void testUrlParsing_data();
     void testUrlParsing();
 
+    void testButtonUrl_data();
+    void testButtonUrl();
+
 private:
     KUrlNavigator *m_navigator;
 };

commit d34b37b6831429f7a48edec7de77cb88524b3784

Fix wrong button in KUrlNavigator issue caused by 9dbe36f
9dbe36f734b5b839b2a6a934fad29d639e954498 breaks some assumption on
startIndex. The argument index passed to buttonUrl is based number of
'/'in full url. Because of that, url like "sftp://a@b.com/a/b" is now
shown as "sftp:a@b.com" / "b" / "b" in dolphin url bar.

This patch changes all relevant code that calls buttonUrl() to use
url.path() instead of url.toDisplayString() to count the number of "/".

REVIEW: 126295

src/filewidgets/kurlnavigator.cpp
@@ -147,13 +147,13 @@ public:
     void deleteButtons();
 
     /**
-     * Retrieves the place path for the current path.
+     * Retrieves the place url for the current url.
      * E. g. for the path "fish://root@192.168.0.2/var/lib" the string
      * "fish://root@192.168.0.2" will be returned, which leads to the
      * navigation indication 'Custom Path > var > lib". For e. g.
      * "settings:///System/" the path "settings://" will be returned.
      */
-    QString retrievePlacePath() const;
+    QUrl retrievePlaceUrl() const;
 
     /**
      * Returns true, if the MIME type of the path represents a
@@ -358,14 +358,18 @@ void KUrlNavigator::Private::openPathSelectorMenu()
     QPointer<QMenu> popup = new QMenu(q);
     popup->setLayoutDirection(Qt::LeftToRight);
 
-    const QString placePath = retrievePlacePath();
-    int idx = placePath.count(QLatin1Char('/')); // idx points to the first directory
+    const QUrl placeUrl = retrievePlaceUrl();
+    int idx = placeUrl.path().count(QLatin1Char('/')); // idx points to the first directory
     // after the place path
 
-    const QString path = m_history[m_historyIndex].url.toDisplayString(QUrl::PreferLocalFile);
+    const QString path = m_history[m_historyIndex].url.path();
     QString dirName = path.section(QLatin1Char('/'), idx, idx);
     if (dirName.isEmpty()) {
-        dirName = QLatin1Char('/');
+        if (placeUrl.isLocalFile()) {
+            dirName = QLatin1String("/");
+        } else {
+            dirName = placeUrl.toDisplayString();
+        }
     }
     do {
         const QString text = spacer + dirName;
@@ -540,7 +544,10 @@ void KUrlNavigator::Private::updateContent()
             placeUrl = m_placesSelector->selectedPlaceUrl();
         }
 
-        QString placePath = placeUrl.isValid() ? placeUrl.toDisplayString(QUrl::PreferLocalFile) : retrievePlacePath();
+        if (!placeUrl.isValid()) {
+            placeUrl = retrievePlaceUrl();
+        }
+        QString placePath = placeUrl.path();
         removeTrailingSlash(placePath);
 
         const int startIndex = placePath.count('/');
@@ -555,7 +562,7 @@ void KUrlNavigator::Private::updateButtons(int startIndex)
         return;
     }
 
-    const QString path = currentUrl.toDisplayString(QUrl::PreferLocalFile);
+    const QString path = currentUrl.path();
 
     bool createButton = false;
     const int oldButtonCount = m_navButtons.count();
@@ -771,21 +778,11 @@ void KUrlNavigator::Private::deleteButtons()
     m_navButtons.clear();
 }
 
-QString KUrlNavigator::Private::retrievePlacePath() const
+QUrl KUrlNavigator::Private::retrievePlaceUrl() const
 {
-    const QUrl currentUrl = q->locationUrl();
-    const QString path = currentUrl.toDisplayString(QUrl::PreferLocalFile);
-    int idx = path.indexOf(QLatin1String("///"));
-    if (idx >= 0) {
-        idx += 3;
-    } else {
-        idx = path.indexOf(QLatin1String("//"));
-        idx = path.indexOf(QLatin1Char('/'), (idx < 0) ? 0 : idx + 2);
-    }
-
-    QString placePath = (idx < 0) ? path : path.left(idx);
-    removeTrailingSlash(placePath);
-    return placePath;
+    QUrl currentUrl = q->locationUrl();
+    currentUrl.setPath(QString());
+    return currentUrl;
 }
 
 bool KUrlNavigator::Private::isCompressedPath(const QUrl &url) const