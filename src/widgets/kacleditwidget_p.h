d8bb995623c255e87399a576ab7b3837d6acd12a
David Faure <faure@kde.org>
2016-04-19 22:33:14 +0200

    Fix clang warning: remove useless infinite-recursion functions

--- src/widgets/kacleditwidget_p.h
+++ src/widgets/kacleditwidget_p.h
@@ -101,16 +101,7 @@ public:
     QStringList allowedUsers(bool defaults, KACLListViewItem *allowedItem = 0);
     QStringList allowedGroups(bool defaults, KACLListViewItem *allowedItem = 0);
 
-    const KACL getACL() const
-    {
-        return getACL();
-    }
     KACL getACL();
-
-    const KACL getDefaultACL() const
-    {
-        return getDefaultACL();
-    }
     KACL getDefaultACL();
 
     QPixmap getYesPixmap() const
