c00a33bc8cd46a1fa0ad3c318d809d87faa50a17
David Faure <faure@kde.org>
2017-02-13 12:14:58 +0100

    GIT_SILENT remove unused member variable (thanks clang)

--- src/ioslaves/trash/trashimpl.h
+++ src/ioslaves/trash/trashimpl.h
@@ -204,7 +204,6 @@ private:
     mutable TrashDirMap m_topDirectories; // id -> $topdir of partition
     dev_t m_homeDevice;
     mutable bool m_trashDirectoriesScanned;
-    int m_mibEnum;
 
     mutable KConfig m_config;
 
