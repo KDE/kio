fb4ad46b0dbacba5a9f184049643f6b29a3dd9e8
Milian Wolff <mail@milianw.de>
2017-03-04 20:04:26 +0100

    With this change it is now possible to list multiple paths as defined via the command line.
    While at it, I refactored the code to clean it up:
    
    - rely on QEventLoopLocker to quit the application once all jobs have finished
    - use a lambda to count the listed entries
    - don't append to a KIO::UDSEntryList to cound the listed entries
    
    REVIEW: 126894

--- tests/listjobtest.cpp
+++ tests/listjobtest.cpp
@@ -23,57 +23,32 @@
 
 #include <QCoreApplication>
 #include <QDebug>
-#include <QElapsedTimer>
-
-class Test : public QObject
-{
-    Q_OBJECT
-
-public:
-    Test() : QObject() {}
-
-public Q_SLOTS:
-    void slotEntries(KIO::Job*, const KIO::UDSEntryList &entries)
-    {
-        m_entries += entries;
-        qDebug() << "Listed" << m_entries.count() << "files.";
-    }
-
-private:
-    KIO::UDSEntryList m_entries;
-};
-
+#include <QDir>
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
+    if (argc < 2) {
         qWarning() << "Expected a path or URL.";
         return 1;
     }
 
     QCoreApplication app(argc, argv);
-    Test testObject;
+    quint64 entriesListed = 0;
 
-    QUrl url = QUrl::fromUserInput(QString::fromLocal8Bit(argv[1]));
-    qDebug() << "Starting listJob for the URL:" << url;
+    for (int i = 1; i < argc; ++i) {
+        QUrl url = QUrl::fromUserInput(QString::fromLocal8Bit(argv[i]), QDir::currentPath());
+        qDebug() << "Starting listJob for the URL:" << url;
 
-    QElapsedTimer timer;
-    timer.start();
+        KIO::ListJob *job = KIO::listDir(url, KIO::HideProgressInfo);
+        job->setUiDelegate(nullptr);
+        job->addMetaData(QStringLiteral("details"), QStringLiteral("2")); // Default is 2 which means all details. 0 means just a few essential fields (KIO::UDSEntry::UDS_NAME, KIO::UDSEntry::UDS_FILE_TYPE and KIO::UDSEntry::UDS_LINK_DEST if it is a symbolic link. Not provided otherwise.
 
-    KIO::ListJob *job = KIO::listDir(url, KIO::HideProgressInfo);
-    job->setUiDelegate(nullptr);
-    job->addMetaData(QStringLiteral("details"), QStringLiteral("2")); // Default is 2 which means all details. 0 means just a few essential fields (KIO::UDSEntry::UDS_NAME, KIO::UDSEntry::UDS_FILE_TYPE and KIO::UDSEntry::UDS_LINK_DEST if it is a symbolic link. Not provided otherwise.
+        QObject::connect(job, &KIO::ListJob::entries,
+                         [&entriesListed] (KIO::Job*, const KIO::UDSEntryList &entries) {
+                            entriesListed += entries.size();
+                            qDebug() << "Listed" << entriesListed << "files.";
+                         });
+    }
 
-    QObject::connect(job, SIGNAL(entries(KIO::Job*,KIO::UDSEntryList)), &testObject, SLOT(slotEntries(KIO::Job*,KIO::UDSEntryList)));
-    QObject::connect(job, SIGNAL(finished(KJob*)), &app, SLOT(quit()));
-
-    int result = app.exec();
-
-    qDebug() << "Runtime:" << timer.elapsed() << "milliseconds.";
-    qDebug() << "Press Enter to quit.";
-    std::cin.ignore();
-
-    return result;
+    return app.exec();
 }
-
-#include "listjobtest.moc"
