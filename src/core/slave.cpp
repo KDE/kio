KDE/kio

commit c85a8e03f7687555cd01b7f85086dde5d4e966fa

[kiocore] Check that klauncher is running or not
Summary:
We are currently checking if the klauncher is running under different
user, and if so, we decide to fork the slave.

In addition to that also check that klauncher is actually running or
not. if klauncher is not installed due to kinit being higher tier
framework then the kio or due to the usage of kio slave on !plasma
environments where kinit is very less likely to be installed.

This also helps packagers to avoid the circular dependency between kio
and kinit. For example,

- https://phabricator.kde.org/T4517
- https://bugs.launchpad.net/ubuntu/+source/kio/+bug/1504526
- https://bugs.archlinux.org/task/49249

Test Plan: Tested on openbox session where kdeinit5 was not functional

Reviewers: #frameworks, sitter, dfaure

Reviewed By: sitter, dfaure

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D7462

src/core/slave.cpp
@@ -100,11 +100,12 @@ static bool forkSlaves()
         }
 
 #ifdef Q_OS_UNIX
-        // fallback: if there's an klauncher process owned by a different user: still fork
         if (!fork) {
             // check the UID of klauncher
             QDBusReply<uint> reply = QDBusConnection::sessionBus().interface()->serviceUid(klauncher()->service());
-            if (reply.isValid() && getuid() != reply) {
+            // if reply is not valid, fork, most likely klauncher can not be run or is not installed
+            // fallback: if there's an klauncher process owned by a different user: still fork
+            if (!reply.isValid() && getuid() != reply) {
                 fork = true;
             }
         }

commit e72b70a064916310435fa19e0740f376bb687e4c

[kiocore] Fix the logic error in previous commit
This fixes the logic error introduced in previous commit
c85a8e03f7687555, Sorry.

src/core/slave.cpp
@@ -105,7 +105,7 @@ static bool forkSlaves()
             QDBusReply<uint> reply = QDBusConnection::sessionBus().interface()->serviceUid(klauncher()->service());
             // if reply is not valid, fork, most likely klauncher can not be run or is not installed
             // fallback: if there's an klauncher process owned by a different user: still fork
-            if (!reply.isValid() && getuid() != reply) {
+            if (!reply.isValid() || getuid() != reply) {
                 fork = true;
             }
         }