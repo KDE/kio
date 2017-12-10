KDE/kio

commit 23874cab9d192a12f3f9f0b261fc6e0784354c1f

Avoid slang in the translatable messages

src/kcms/kio/kcookiespolicyselectiondlg.h
@@ -35,13 +35,13 @@ public:
         case KCookieAdvice::Accept:
             return I18N_NOOP ("Accept");
         case KCookieAdvice::AcceptForSession:
-            return I18N_NOOP ("AcceptForSession");
+            return I18N_NOOP ("Accept For Session");
         case KCookieAdvice::Reject:
             return I18N_NOOP ("Reject");
         case KCookieAdvice::Ask:
             return I18N_NOOP ("Ask");
         default:
-            return I18N_NOOP ("Dunno");
+            return I18N_NOOP ("Do Not Know");
         }
     }
 

commit efeda6c1cc905913ef8cf587d966264d0135aba0

fix handling of KCookieAdvice::AcceptForSession
Commit 23874cab9d broke the handling of KCookieAdvice::AcceptForSession
because strToAdvice(adviceToStr(KCookieAdvice::AcceptForSession)) now
returns KCookieAdvice::Dunno. Ignore spaces in the argument of
strToAdvice to properly recognize the AcceptForSession setting and
restore
compatibility with previous configs.

BUG: 386325

src/kcms/kio/kcookiespolicyselectiondlg.h
@@ -49,7 +49,7 @@ public:
         if (_str.isEmpty())
             return KCookieAdvice::Dunno;
 
-        QString advice = _str.toLower();
+        QString advice = _str.toLower().remove(' ');
 
         if (advice == QLatin1String ("accept"))
             return KCookieAdvice::Accept;