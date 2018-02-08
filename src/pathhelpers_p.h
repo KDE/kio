eb27e2618d3103f003b06cab8eccf352b27764a0
David Faure <faure@kde.org>
2018-02-08 21:35:34 +0100

    Disable assert for 5.43, I've been hitting it a few times in the file dialog
    I'll need to find out how this happens exactly, but for now, this will
    prevent angry users.
    
    CCMAIL: bvbfan@abv.bg

--- src/pathhelpers_p.h
+++ src/pathhelpers_p.h
@@ -28,7 +28,7 @@
 inline
 QString concatPaths(const QString &path1, const QString &path2)
 {
-    Q_ASSERT(!path2.startsWith(QLatin1Char('/')));
+    // TODO Q_ASSERT(!path2.startsWith(QLatin1Char('/')));
 
     if (!path1.endsWith(QLatin1Char('/'))) {
         return path1 + QLatin1Char('/') + path2;
