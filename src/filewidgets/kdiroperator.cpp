KDE/kio

commit 0792ed68923b3c3eac6fd3b9769e83ed2de9842b

Use F11 as the shortcut to toggle the aside preview
Summary:
FEATURE: 389880
FIXED-IN: 5.44

Test Plan:
{F5692446}

Shortcut works and toggles the aside preview.

However, in doing so, another pre-existing bug is triggered: the file dialog doesn't eat the keyboard input, so if the app that spawned it has something bound to F11, you'll erroneously get the "Ambiguous shortcut" message (even though the keystroke is/should be passed only to the file dialog, not the host app). Help solving this would be appreciated.

Reviewers: #frameworks, markg

Reviewed By: markg

Subscribers: markg

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D10314

src/filewidgets/kdiroperator.cpp
@@ -1949,6 +1949,7 @@ void KDirOperator::setupActions()
 
     KToggleAction *previewAction = new KToggleAction(i18n("Show Aside Preview"), this);
     d->actionCollection->addAction(QStringLiteral("preview"), previewAction);
+    previewAction->setShortcut(Qt::Key_F11);
     connect(previewAction, SIGNAL(toggled(bool)),
             SLOT(_k_togglePreview(bool)));
 

commit c2a80e71b39d0e3a034588ebad7ed9b34ff80ce2

Fix "ambiguous shortcut" issue introduced with D10314
Summary:
My patch D10314 was insufficiently tested (sorry about that!) and introduced an "Ambiguous Shortcut" warning, because "show/hide inline previews" was already bound to F11.

This patch solves that issue by changing the shortcut to F12. F11 was never appropriate for it in the first place, since that's used for something different in Dolphin anyway. Dolphin doesn't currently have a shortcut for "show hide inline previews", so there's nothing to be consistent with. I used F12 in the file dialogs since it's unused in both places, so we could add it to Dolphin too if we really wanted.

Test Plan: Tested with Kate. F11 toggles the aside preview, and F12 toggles the inline preview, like they should. No more conflicts!

Reviewers: #frameworks, #dolphin, markg, elvisangelaccio

Reviewed By: markg

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D10617

src/filewidgets/kdiroperator.cpp
@@ -1968,6 +1968,7 @@ void KDirOperator::setupActions()
     KToggleAction *inlinePreview = new KToggleAction(QIcon::fromTheme(QStringLiteral("view-preview")),
             i18n("Show Preview"), this);
     d->actionCollection->addAction(QStringLiteral("inline preview"), inlinePreview);
+    inlinePreview->setShortcut(Qt::Key_F12);
     connect(inlinePreview, SIGNAL(toggled(bool)), SLOT(_k_toggleInlinePreviews(bool)));
 
     QAction *fileManager = new QAction(i18n("Open File Manager"), this);

src/filewidgets/kfilewidget.cpp
@@ -499,7 +499,7 @@ KFileWidget::KFileWidget(const QUrl &_startDir, QWidget *parent)
     menu->addAction(showHidden);
     menu->addAction(showSidebarAction);
     menu->addAction(showBookmarksAction);
-    coll->action(QStringLiteral("inline preview"))->setShortcut(QKeySequence(Qt::Key_F11));
+    coll->action(QStringLiteral("inline preview"));
     menu->addAction(coll->action(QStringLiteral("preview")));
 
     menu->setDelayed(false);