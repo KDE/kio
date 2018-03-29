KDE/kio

commit d7cce9937d5e9af2753fadb82d11f308b58bb8fa

Support NTFS hidden files
Summary:
This patch implements function `isHiddenNtfs` which checks extended attribute `HIDDEN` of files from NTFS partitions and populates `UDSEntry` with `KIO::UDSEntry::UDS_HIDDEN`.
FEATURE: 171537

{F5747912}

Test Plan:
# Open Dolphin on Windows partition
# Check that "$Recycle.Bin", "MSOCache", "System Volume Information", etc. are hidden
# Show hidden files
# Check that Windows hidden files are displayed now

Reviewers: #dolphin, #frameworks, markg, dfaure

Reviewed By: dfaure

Subscribers: dfaure, markg, elvisangelaccio, ltoscano, anthonyfieroni, broulik, #frameworks, #dolphin

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D11204

src/ioslaves/file/file_unix.cpp
@@ -37,6 +37,7 @@
 #include <kmountpoint.h>
 
 #include <errno.h>
+#include <sys/xattr.h>
 #include <utime.h>
 
 #include <KAuth>
@@ -410,6 +411,44 @@ static bool isLocalFileSameHost(const QUrl &url)
     return (QString::compare(url.host(), QLatin1String(hostname), Qt::CaseInsensitive) == 0);
 }
 
+#ifdef Q_OS_LINUX
+static bool isNtfsHidden(const QString &filename)
+{
+    constexpr auto attrName = "system.ntfs_attrib_be";
+    const auto filenameEncoded = QFile::encodeName(filename);
+    auto length = getxattr(filenameEncoded.data(), attrName, nullptr, 0);
+    if (length <= 0) {
+        return false;
+    }
+    constexpr size_t xattr_size = 1024;
+    char strAttr[xattr_size];
+    length = getxattr(filenameEncoded.data(), attrName, strAttr, xattr_size);
+    if (length <= 0) {
+        return false;
+    }
+
+    // Decode result to hex string
+    static constexpr auto digits = "0123456789abcdef";
+    QVarLengthArray<char> hexAttr(static_cast<int>(length) * 2 + 4);
+    char *c = strAttr;
+    char *e = hexAttr.data();
+    *e++ ='0';
+    *e++ = 'x';
+    for (auto n = 0; n < length; n++, c++) {
+        *e++ = digits[(static_cast<uchar>(*c) >> 4)];
+        *e++ = digits[(static_cast<uchar>(*c) & 0x0F)];
+    }
+    *e = '\0';
+
+    // Decode hex string to int
+    auto intAttr = static_cast<uint>(strtol(hexAttr.data(), nullptr, 16));
+
+    constexpr auto FILE_ATTRIBUTE_HIDDEN = 0x2u;
+    return static_cast<bool>(intAttr & FILE_ATTRIBUTE_HIDDEN);
+}
+#endif
+
+
 void FileProtocol::listDir(const QUrl &url)
 {
     if (!isLocalFileSameHost(url)) {
@@ -506,6 +545,11 @@ void FileProtocol::listDir(const QUrl &url)
 
         } else {
             if (createUDSEntry(filename, QByteArray(ep->d_name), entry, details)) {
+#ifdef Q_OS_LINUX
+                if (isNtfsHidden(filename)) {
+                    entry.insert(KIO::UDSEntry::UDS_HIDDEN, 1);
+                }
+#endif
                 listEntry(entry);
             }
         }

commit f1ab805c4ebcea24f0b84b785bc56515060e5eb2

Fix build on FreeBSD broken by d7cce9937d5e9af2753fadb82d11f308b58bb8fa
Summary: Support hidden NTFS files only on OS that support extended file attributes. For this purpose check if `sys/xattr.h` exist via CMake.

Reviewers: #dolphin, kossebau, dfaure

Reviewed By: dfaure

Subscribers: ngraham, bcooksley, #frameworks

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D11716

src/ioslaves/file/CMakeLists.txt
@@ -18,6 +18,8 @@ set(HAVE_POSIX_ACL ${ACL_FOUND})
 set_package_properties(ACL PROPERTIES DESCRIPTION "LibACL" URL "ftp://oss.sgi.com/projects/xfs/cmd_tars"
                        TYPE RECOMMENDED PURPOSE "Support for manipulating access control lists")
 
+check_include_files(sys/xattr.h HAVE_SYS_XATTR_H)
+
 configure_file(config-kioslave-file.h.cmake ${CMAKE_CURRENT_BINARY_DIR}/config-kioslave-file.h )
 
 add_library(kio_file MODULE ${kio_file_PART_SRCS})

src/ioslaves/file/config-kioslave-file.h.cmake
@@ -10,3 +10,6 @@
 /* Defined to if you have a d_type member in struct dirent */
 #cmakedefine01 HAVE_DIRENT_D_TYPE
 
+/* Defined if system has extended file attributes support. */
+#cmakedefine01 HAVE_SYS_XATTR_H
+

src/ioslaves/file/file_unix.cpp
@@ -37,7 +37,9 @@
 #include <kmountpoint.h>
 
 #include <errno.h>
+#if HAVE_SYS_XATTR_H
 #include <sys/xattr.h>
+#endif
 #include <utime.h>
 
 #include <KAuth>
@@ -411,7 +413,7 @@ static bool isLocalFileSameHost(const QUrl &url)
     return (QString::compare(url.host(), QLatin1String(hostname), Qt::CaseInsensitive) == 0);
 }
 
-#ifdef Q_OS_LINUX
+#if HAVE_SYS_XATTR_H
 static bool isNtfsHidden(const QString &filename)
 {
     constexpr auto attrName = "system.ntfs_attrib_be";
@@ -545,7 +547,7 @@ void FileProtocol::listDir(const QUrl &url)
 
         } else {
             if (createUDSEntry(filename, QByteArray(ep->d_name), entry, details)) {
-#ifdef Q_OS_LINUX
+#if HAVE_SYS_XATTR_H
                 if (isNtfsHidden(filename)) {
                     entry.insert(KIO::UDSEntry::UDS_HIDDEN, 1);
                 }