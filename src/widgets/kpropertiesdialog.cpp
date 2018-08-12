5d8c9c9d929d20906838110d6489bde4b28c3548
Shubham Jangra <aryan100jangid@gmail.com>
2018-08-12 12:49:05 +0530

    Display sorry message if file is not local

--- src/widgets/kpropertiesdialog.cpp
+++ src/widgets/kpropertiesdialog.cpp
@@ -3114,7 +3114,7 @@ void KUrlPropsPlugin::applyChanges()
     const QUrl url = job->mostLocalUrl();
 
     if (!url.isLocalFile()) {
-        //FIXME: 4.2 add this: KMessageBox::sorry(0, i18n("Could not save properties. Only entries on local file systems are supported."));
+        KMessageBox::sorry(0, i18n("Could not save properties. Only entries on local file systems are supported."));
         return;
     }
 
