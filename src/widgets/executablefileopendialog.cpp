89557a218d7b9fa26a9ed6c9dfca5fe903bf928f
Nate Graham <nate@kde.org>
2019-03-31 13:01:58 -0600

    Improve executable dialog buttons' icons
    Summary:
    The buttons were using mimetype icons, which don't change their colors to reflect the theme, and aren't designed for display at those sizes inside buttons.
    
    BUG: 406090
    FIXED-IN: 5.57
    
    Test Plan:
    Before, light: {F6739736, size=full}
    After, light: {F6739737, size=full}
    Before, dark: {F6739744, size=full}
    After, dark: {F6739746, size=full}
    
    Reviewers: #vdg, GB_2, ndavis
    
    Reviewed By: #vdg, GB_2, ndavis
    
    Subscribers: ndavis, kde-frameworks-devel
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D20147

--- src/widgets/executablefileopendialog.cpp
+++ src/widgets/executablefileopendialog.cpp
@@ -38,8 +38,8 @@ ExecutableFileOpenDialog::ExecutableFileOpenDialog(QWidget *parent) :
     QPushButton *openButton = new QPushButton(i18n("&Open"), this);
     QPushButton *executeButton = new QPushButton(i18n("&Execute"), this);
 
-    openButton->setIcon(QIcon::fromTheme(QStringLiteral("text-plain")));
-    executeButton->setIcon(QIcon::fromTheme(QStringLiteral("application-x-executable")));
+    openButton->setIcon(QIcon::fromTheme(QStringLiteral("document-preview")));
+    executeButton->setIcon(QIcon::fromTheme(QStringLiteral("system-run")));
 
     QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Cancel, this);
     buttonBox->addButton(openButton, QDialogButtonBox::AcceptRole);
