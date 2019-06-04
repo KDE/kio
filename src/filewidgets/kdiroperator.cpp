KDE/kio

commit f17749e70c09eb7f3a52ef6ee5f3c90327319273

Reword some text, a couple clean ups, add a separator
Summary: Re-word some menu items per T8552#140370

Test Plan:
before:
{F6793291}

after
{F6793290}

Reviewers: ngraham, #frameworks, #vdg, GB_2

Reviewed By: ngraham, #vdg, GB_2

Subscribers: kde-frameworks-devel

Tags: #frameworks, #vdg

Differential Revision: https://phabricator.kde.org/D20827

src/filewidgets/kdiroperator.cpp
@@ -1915,12 +1915,6 @@ void KDirOperator::setupActions()
     d->actionCollection->addAction(QStringLiteral("dirs first"), dirsFirstAction);
     connect(dirsFirstAction, SIGNAL(triggered(bool)), this, SLOT(_k_slotToggleDirsFirst()));
 
-    QActionGroup *sortGroup = new QActionGroup(this);
-    byNameAction->setActionGroup(sortGroup);
-    bySizeAction->setActionGroup(sortGroup);
-    byDateAction->setActionGroup(sortGroup);
-    byTypeAction->setActionGroup(sortGroup);
-
     d->decorationMenu = new KActionMenu(i18n("Icon Position"), this);
     d->actionCollection->addAction(QStringLiteral("decoration menu"), d->decorationMenu);
 
@@ -1970,7 +1964,7 @@ void KDirOperator::setupActions()
     showHiddenAction->setShortcuts({Qt::ALT + Qt::Key_Period, Qt::CTRL + Qt::Key_H, Qt::Key_F8});
     connect(showHiddenAction, SIGNAL(toggled(bool)), SLOT(_k_slotToggleHidden(bool)));
 
-    KToggleAction *previewAction = new KToggleAction(i18n("Show Aside Preview"), this);
+    KToggleAction *previewAction = new KToggleAction(i18n("Show Preview Panel"), this);
     d->actionCollection->addAction(QStringLiteral("preview"), previewAction);
     previewAction->setShortcut(Qt::Key_F11);
     connect(previewAction, SIGNAL(toggled(bool)),

src/filewidgets/kfilewidget.cpp
@@ -474,14 +474,14 @@ KFileWidget::KFileWidget(const QUrl &_startDir, QWidget *parent)
     goToNavigatorAction->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_L));
 
     KToggleAction *showSidebarAction =
-        new KToggleAction(i18n("Show Places Navigation Panel"), this);
+        new KToggleAction(i18n("Show Places Panel"), this);
     coll->addAction(QStringLiteral("toggleSpeedbar"), showSidebarAction);
     showSidebarAction->setShortcut(QKeySequence(Qt::Key_F9));
     connect(showSidebarAction, SIGNAL(toggled(bool)),
             SLOT(_k_toggleSpeedbar(bool)));
 
     KToggleAction *showBookmarksAction =
-        new KToggleAction(i18n("Show Bookmarks"), this);
+        new KToggleAction(i18n("Show Bookmarks Button"), this);
     coll->addAction(QStringLiteral("toggleBookmarks"), showBookmarksAction);
     connect(showBookmarksAction, SIGNAL(toggled(bool)),
             SLOT(_k_toggleBookmarks(bool)));
@@ -493,7 +493,7 @@ KFileWidget::KFileWidget(const QUrl &_startDir, QWidget *parent)
                             "<li>how files are sorted in the list</li>"
                             "<li>types of view, including icon and list</li>"
                             "<li>showing of hidden files</li>"
-                            "<li>the Places navigation panel</li>"
+                            "<li>the Places panel</li>"
                             "<li>file previews</li>"
                             "<li>separating folders from files</li></ul></qt>"));
     menu->addAction(coll->action(QStringLiteral("view menu")));
@@ -501,9 +501,9 @@ KFileWidget::KFileWidget(const QUrl &_startDir, QWidget *parent)
     menu->addAction(coll->action(QStringLiteral("decoration menu")));
     menu->addSeparator();
     menu->addAction(coll->action(QStringLiteral("show hidden")));
+    menu->addSeparator();
     menu->addAction(showSidebarAction);
     menu->addAction(showBookmarksAction);
-    coll->action(QStringLiteral("inline preview"));
     menu->addAction(coll->action(QStringLiteral("preview")));
 
     menu->setDelayed(false);

commit 3933e9bf17966153e32632676aca23c0d33c253e

Restore exclusive group for sorting menu items Otherwise the items in the sorting menu have checkboxes instead of radio buttons and more than one can be selected at once, which makes no sense
Bug introduced with f17749e70c09eb7f3a52ef6ee5f3c90327319273 and I
didn't catch it in the review, sorry about that

CCMAIL: meven29@gmail.com

src/filewidgets/kdiroperator.cpp
@@ -1974,6 +1974,12 @@ void KDirOperator::setupActions()
     compactViewAction->setActionGroup(viewModeGroup);
     detailsViewAction->setActionGroup(viewModeGroup);
 
+    QActionGroup *sortGroup = new QActionGroup(this);
+    byNameAction->setActionGroup(sortGroup);
+    bySizeAction->setActionGroup(sortGroup);
+    byDateAction->setActionGroup(sortGroup);
+    byTypeAction->setActionGroup(sortGroup);
+
     d->decorationMenu = new KActionMenu(i18n("Icon Position"), this);
     d->actionCollection->addAction(QStringLiteral("decoration menu"), d->decorationMenu);
 