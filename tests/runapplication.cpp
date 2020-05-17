f6849fde97f78a42f19bdc3c764622686c30542b
David Faure <faure@kde.org>
2020-05-18 00:35:22 +0200

    Port runapplication to ApplicationLauncherJob
    NO_CHANGELOG

--- tests/runapplication.cpp
+++ tests/runapplication.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the KDE libraries
     Copyright (c) 1999 Waldo Bastian <bastian@kde.org>
-    Copyright (c) 2009 David Faure <faure@kde.org>
+    Copyright (c) 2009, 2020 David Faure <faure@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -17,17 +17,17 @@
     Boston, MA 02110-1301, USA.
 */
 
-#include <KRun>
 #include <KService>
+#include <KIO/ApplicationLauncherJob>
+#include <KIO/JobUiDelegate>
 #include <QApplication>
 #include <QDebug>
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
-    QString serviceId = QStringLiteral("kwrite.desktop");
+    QString serviceId = QStringLiteral("org.kde.kwrite");
     if (argc > 1) {
         serviceId = QString::fromLocal8Bit(argv[1]);
     }
@@ -36,12 +36,27 @@ main(int argc, char *argv[])
         urls << QUrl::fromUserInput(QString::fromLocal8Bit(argv[2]));
     }
 
-    KService::Ptr service = KService::serviceByDesktopPath(serviceId);
+    KService::Ptr service = KService::serviceByDesktopName(serviceId);
     if (!service) {
-        service = new KService(serviceId);
+        service = KService::serviceByStorageId(serviceId + QLatin1String(".desktop"));
+        if (!service) {
+            qWarning() << "Service not found" << serviceId;
+            return 1;
+        }
     }
-    qint64 pid = KRun::runApplication(*service, urls, nullptr);
-    qDebug() << "Started. pid=" << pid;
 
-    return 0;
+    KIO::ApplicationLauncherJob *job = new KIO::ApplicationLauncherJob(service);
+    job->setUrls(urls);
+    job->setUiDelegate(new KIO::JobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, nullptr));
+    job->start();
+
+    QObject::connect(job, &KJob::result, &app, [&]() {
+        if (job->error()) {
+            app.exit(1);
+        } else {
+            qDebug() << "Started. pid=" << job->pid();
+        }
+    });
+
+    return app.exec();
 }
