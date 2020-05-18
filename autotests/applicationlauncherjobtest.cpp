KDE/kio

commit 6452a34cf01d03d3167b38bc28a2fe8e13569021

Fix service file specifying 'Run in terminal' giving an error code 100
Happens because KIO::DesktopExecParser::resultingArguments() prepends
the terminal application to the command line.  If this is a relative path,
as it is most likely to be (and will be in the default 'konsole' case),
the "realExecutable" check in KProcessRunner::KProcessRunner() is triggered
and the job aborts with an error.

Expand the specified terminal executable into a full path in resultingArguments(),
and return an error immediately if it cannot be found.  This full terminal path
is not relative for the KProcessRunner::KProcessRunner() check and does not fail.

Check that resultingArguments() is not empty (an error return) before accessing
the first word of the command, so that it does not assert if the list is empty.
Also only call resultingArguments() once.

BUG: 421374
FIXED-IN: 5.71

Differential Revision: https://phabricator.kde.org/D29738

src/core/desktopexecparser.cpp
@@ -421,7 +421,15 @@ QStringList KIO::DesktopExecParser::resultingArguments() const
     if (d->service.terminal()) {
         KConfigGroup cg(KSharedConfig::openConfig(), "General");
         QString terminal = cg.readPathEntry("TerminalApplication", QStringLiteral("konsole"));
-        if (terminal == QLatin1String("konsole")) {
+        const bool isKonsole = (terminal == QLatin1String("konsole"));
+
+        QString terminalPath = QStandardPaths::findExecutable(terminal);
+        if (terminalPath.isEmpty()) {
+            qCWarning(KIO_CORE) << "Terminal" << terminal << "not found, service" << d->service.name();
+            return QStringList();
+        }
+        terminal = terminalPath;
+        if (isKonsole) {
             if (!d->service.workingDirectory().isEmpty()) {
                 terminal += QLatin1String(" --workdir ") + KShell::quoteArg(d->service.workingDirectory());
             }

src/gui/kprocessrunner.cpp
@@ -88,9 +88,17 @@ KProcessRunner::KProcessRunner(const KService::Ptr &service, const QList<QUrl> &
         emitDelayedError(i18n("The desktop entry file\n%1\nis not valid.", service->entryPath()));
         return;
     }
-    KIO::DesktopExecParser execParser(*service, urls);
 
-    const QString realExecutable = execParser.resultingArguments().at(0);
+    KIO::DesktopExecParser execParser(*service, urls);
+    execParser.setUrlsAreTempFiles(flags & KIO::ApplicationLauncherJob::DeleteTemporaryFiles);
+    execParser.setSuggestedFileName(suggestedFileName);
+    const QStringList args = execParser.resultingArguments();
+    if (args.isEmpty()) {
+        emitDelayedError(i18n("Error processing Exec field in %1", service->entryPath()));
+        return;
+    }
+
+    const QString realExecutable = args.at(0);
     // realExecutable is a full path if DesktopExecParser was able to locate it. Otherwise it's still relative, which is a bad sign.
     if (QDir::isRelativePath(realExecutable) || !QFileInfo(realExecutable).isExecutable()) {
         // Does it really not exist, or is it non-executable? (bug #415567)
@@ -103,13 +111,6 @@ KProcessRunner::KProcessRunner(const KService::Ptr &service, const QList<QUrl> &
         return;
     }
 
-    execParser.setUrlsAreTempFiles(flags & KIO::ApplicationLauncherJob::DeleteTemporaryFiles);
-    execParser.setSuggestedFileName(suggestedFileName);
-    const QStringList args = execParser.resultingArguments();
-    if (args.isEmpty()) {
-        emitDelayedError(i18n("Error processing Exec field in %1", service->entryPath()));
-        return;
-    }
     //qDebug() << "KProcess args=" << args;
     *m_process << args;
 

commit d1e9325fba37eddb9cb44173edfc1fee122a0c57

Make sure the program exists and is executable before we inject kioexec/kdesu/etc.
This consolidates all the logic for finding the executable at the
beginning of DesktopExecParser, rather than half in DesktopExecParser
(too late, after injecting kioexec into the command) and half in
KProcessRunner (which means other DesktopExecParser users didn't benefit
from it, and we had to guess what happened in DesktopExecParser)

This fixes applicationlauncherjobtest's regression after
commit 6452a34cf01d03d3167b38bc28a2fe8e13569021.

autotests/applicationlauncherjobtest.cpp
@@ -125,7 +125,7 @@ void ApplicationLauncherJobTest::startProcess()
         job->setRunFlags(KIO::ApplicationLauncherJob::DeleteTemporaryFiles);
     }
     if (useExec) {
-        QVERIFY(job->exec());
+        QVERIFY2(job->exec(), qPrintable(job->errorString()));
     } else {
         job->start();
         QVERIFY(job->waitForStarted());
@@ -286,6 +286,58 @@ void ApplicationLauncherJobTest::shouldFailOnInvalidService()
     QCOMPARE(job->errorString(), QStringLiteral("The desktop entry file\n%1\nis not valid.").arg(desktopFilePath));
 
     QFile::remove(desktopFilePath);
+}
+
+void ApplicationLauncherJobTest::shouldFailOnServiceWithNoExec()
+{
+    const QString desktopFilePath = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + QLatin1String("/kservices5/invalid_service.desktop");
+    KDesktopFile file(desktopFilePath);
+    KConfigGroup group = file.desktopGroup();
+    group.writeEntry("Name", "KRunUnittestServiceNoExec");
+    group.writeEntry("Type", "Service");
+    file.sync();
+
+    QTest::ignoreMessage(QtWarningMsg, qPrintable(QString("No Exec field in \"%1\"").arg(desktopFilePath)));
+    KService::Ptr servicePtr(new KService(desktopFilePath));
+    KIO::ApplicationLauncherJob *job = new KIO::ApplicationLauncherJob(servicePtr, this);
+    QVERIFY(!job->exec());
+    QCOMPARE(job->error(), KJob::UserDefinedError);
+    QCOMPARE(job->errorString(), QStringLiteral("No Exec field in %1").arg(desktopFilePath));
+
+    QFile::remove(desktopFilePath);
+}
+
+void ApplicationLauncherJobTest::shouldFailOnExecutableWithoutPermissions()
+{
+#ifdef Q_OS_UNIX
+    // Given an executable shell script that copies "src" to "dest" (we'll cheat with the mimetype to treat it like a native binary)
+    QTemporaryDir tempDir;
+    const QString dir = tempDir.path();
+    const QString scriptFilePath = dir + QStringLiteral("/script.sh");
+    QFile scriptFile(scriptFilePath);
+    QVERIFY(scriptFile.open(QIODevice::WriteOnly));
+    scriptFile.write("#!/bin/sh\ncp src dest");
+    scriptFile.close();
+    // Note that it's missing executable permissions
+
+    const QString desktopFilePath = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + QLatin1String("/kservices5/invalid_service.desktop");
+    KDesktopFile file(desktopFilePath);
+    KConfigGroup group = file.desktopGroup();
+    group.writeEntry("Name", "KRunUnittestServiceNoPermission");
+    group.writeEntry("Type", "Service");
+    group.writeEntry("Exec", scriptFilePath);
+    file.sync();
+
+    KService::Ptr servicePtr(new KService(desktopFilePath));
+    KIO::ApplicationLauncherJob *job = new KIO::ApplicationLauncherJob(servicePtr, this);
+    QVERIFY(!job->exec());
+    QCOMPARE(job->error(), KJob::UserDefinedError);
+    QCOMPARE(job->errorString(), QStringLiteral("The program '%1' is missing executable permissions.").arg(scriptFilePath));
+
+    QFile::remove(desktopFilePath);
+#else
+    QSKIP("This test is not run on Windows");
+#endif
 }
 
 void ApplicationLauncherJobTest::writeTempServiceDesktopFile(const QString &filePath)

autotests/applicationlauncherjobtest.h
@@ -43,6 +43,8 @@ private Q_SLOTS:
     void shouldFailOnNonExistingExecutable();
 
     void shouldFailOnInvalidService();
+    void shouldFailOnServiceWithNoExec();
+    void shouldFailOnExecutableWithoutPermissions();
 
 private:
     QString createTempService();

autotests/krununittest.cpp
@@ -77,7 +77,7 @@ void KRunUnitTest::testExecutableName_data()
     QTest::newRow("\"progname\" \"arg1\"") << "\"progname\" \"arg1\"" << "progname" << "progname";
     QTest::newRow("'quoted' \"arg1\"") << "'quoted' \"arg1\"" << "quoted" << "quoted";
     QTest::newRow(" 'leading space'   arg1") << " 'leading space'   arg1" << "leading space" << "leading space";
-    QTest::newRow("if_command") << "if test -e /tmp/foo; then kwrite ; else konsole ; fi" << "if" << "if";
+    QTest::newRow("if_command") << "if test -e /tmp/foo; then kwrite ; else konsole ; fi" << "" << ""; // "if" isn't a known executable, so this is good...
 }
 
 void KRunUnitTest::testExecutableName()
@@ -193,11 +193,6 @@ void KRunUnitTest::testProcessDesktopExecNoFile_data()
     QVERIFY(QFileInfo::exists(kioexec));
     QString kioexecQuoted = KShell::quoteArg(kioexec);
 
-    QString kmailservice = QStandardPaths::findExecutable(QStringLiteral("kmailservice5"));
-    if (!QFile::exists(kmailservice)) {
-        kmailservice = QStringLiteral("kmailservice5");
-    }
-
     QTest::newRow("%U l0") << "kdeinit5 %U" << l0 << false << kdeinit;
     QTest::newRow("%U l1") << "kdeinit5 %U" << l1 << false << kdeinit + " /tmp";
     QTest::newRow("%U l2") << "kdeinit5 %U" << l2 << false << kdeinit + " http://localhost/foo";
@@ -219,8 +214,9 @@ void KRunUnitTest::testProcessDesktopExecNoFile_data()
     QTest::newRow("sh -c kdeinit5 %F") << "sh -c \"kdeinit5 \"'\\\"'\"%F\"'\\\"'"
                                        << l1 << false << m_sh + " -c 'kdeinit5 \\\"/tmp\\\"'";
 
-    QTest::newRow("kmailservice5 %u l1") << "kmailservice5 %u" << l1 << false << kmailservice + " /tmp";
-    QTest::newRow("kmailservice5 %u l4") << "kmailservice5 %u" << l4 << false << kmailservice + " http://login:password@www.kde.org";
+    // This was originally with kmailservice5, but that relies on it being installed
+    QTest::newRow("kdeinit5 %u l1") << "kdeinit5 %u" << l1 << false << kdeinit + " /tmp";
+    QTest::newRow("kdeinit5 %u l4") << "kdeinit5 %u" << l4 << false << kdeinit + " http://login:password@www.kde.org";
 }
 
 void KRunUnitTest::testProcessDesktopExecNoFile()
@@ -232,7 +228,9 @@ void KRunUnitTest::testProcessDesktopExecNoFile()
     QFETCH(QString, expected);
     KIO::DesktopExecParser parser(service, urls);
     parser.setUrlsAreTempFiles(tempfiles);
-    QCOMPARE(KShell::joinArgs(parser.resultingArguments()), expected);
+    const QStringList args = parser.resultingArguments();
+    QVERIFY2(!args.isEmpty(), qPrintable(parser.errorMessage()));
+    QCOMPARE(KShell::joinArgs(args), expected);
 }
 
 extern KSERVICE_EXPORT int ksycoca_ms_between_checks;

src/core/desktopexecparser.cpp
@@ -30,6 +30,7 @@
 #include <kconfiggroup.h>
 #include <kprotocolinfo.h>
 #include <kmimetypetrader.h>
+#include <KLocalizedString>
 
 #include <QFile>
 #include <QDir>
@@ -264,6 +265,7 @@ public:
     QList<QUrl> urls;
     bool tempFiles;
     QString suggestedFileName;
+    QString m_errorString;
 };
 
 KIO::DesktopExecParser::DesktopExecParser(const KService &service, const QList<QUrl> &urls)
@@ -294,12 +296,77 @@ static const QString kioexecPath()
     return kioexec;
 }
 
+static QString findNonExecutableProgram(const QString &executable)
+{
+    // Relative to current dir, or absolute path
+    const QFileInfo fi(executable);
+    if (fi.exists() && !fi.isExecutable()) {
+        return executable;
+    }
+
+#ifdef Q_OS_UNIX
+    // This is a *very* simplified version of QStandardPaths::findExecutable
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
+    const auto skipEmptyParts = QString::SkipEmptyParts;
+#else
+    const auto skipEmptyParts = Qt::SkipEmptyParts;
+#endif
+    const QStringList searchPaths = QString::fromLocal8Bit(qgetenv("PATH")).split(QDir::listSeparator(), skipEmptyParts);
+    for (const QString &searchPath : searchPaths) {
+        const QString candidate = searchPath + QLatin1Char('/') + executable;
+        const QFileInfo fileInfo(candidate);
+        if (fileInfo.exists()) {
+            if (fileInfo.isExecutable()) {
+                qWarning() << "Internal program error. QStandardPaths::findExecutable couldn't find" << executable << "but our own logic found it at" << candidate << ". Please report a bug at https://bugs.kde.org";
+            } else {
+                return candidate;
+            }
+        }
+    }
+#endif
+    return QString();
+}
+
 QStringList KIO::DesktopExecParser::resultingArguments() const
 {
     QString exec = d->service.exec();
     if (exec.isEmpty()) {
-        qCWarning(KIO_CORE) << "No Exec field in `" << d->service.entryPath() << "' !";
+        d->m_errorString = i18n("No Exec field in %1", d->service.entryPath());
+        qCWarning(KIO_CORE) << "No Exec field in" << d->service.entryPath();
         return QStringList();
+    }
+
+    // Extract the name of the binary to execute from the full Exec line, to see if it exists
+    const QString binary = executablePath(exec);
+    QString executableFullPath;
+    if (!binary.isEmpty()) { // skip all this if the Exec line is a complex shell command
+        if (QDir::isRelativePath(binary)) {
+            // Resolve the executable to ensure that helpers in libexec are found.
+            // Too bad for commands that need a shell - they must reside in $PATH.
+            executableFullPath = QStandardPaths::findExecutable(binary);
+            qDebug() << "findExecutable(" << binary << ") said" << executableFullPath;
+            if (executableFullPath.isEmpty()) {
+                executableFullPath = QFile::decodeName(CMAKE_INSTALL_FULL_LIBEXECDIR_KF5 "/") + binary;
+            }
+        } else {
+            executableFullPath = binary;
+        }
+
+        // Now check that the binary exists and has the executable flag
+        if (!QFileInfo(executableFullPath).isExecutable()) {
+            // Does it really not exist, or is it non-executable (on Unix)? (bug #415567)
+            const QString nonExecutable = findNonExecutableProgram(binary);
+            if (nonExecutable.isEmpty()) {
+                d->m_errorString = i18n("Could not find the program '%1'", binary);
+            } else {
+                if (QDir::isRelativePath(binary)) {
+                    d->m_errorString = i18n("The program '%1' was found at '%2' but it is missing executable permissions.", binary, nonExecutable);
+                } else {
+                    d->m_errorString = i18n("The program '%1' is missing executable permissions.", nonExecutable);
+                }
+            }
+            return QStringList();
+        }
     }
 
     QStringList result;
@@ -309,6 +376,7 @@ QStringList KIO::DesktopExecParser::resultingArguments() const
     KRunMX2 mx2(d->urls);
 
     if (!mx1.expandMacrosShellQuote(exec)) {    // Error in shell syntax
+        d->m_errorString = i18n("Syntax error in command %1 coming from %2", exec, d->service.entryPath());
         qCWarning(KIO_CORE) << "Syntax error in command" << d->service.exec() << ", service" << d->service.name();
         return QStringList();
     }
@@ -425,6 +493,7 @@ QStringList KIO::DesktopExecParser::resultingArguments() const
 
         QString terminalPath = QStandardPaths::findExecutable(terminal);
         if (terminalPath.isEmpty()) {
+            d->m_errorString = i18n("Terminal %1 not found while trying to run %2", terminal, d->service.entryPath());
             qCWarning(KIO_CORE) << "Terminal" << terminal << "not found, service" << d->service.name();
             return QStringList();
         }
@@ -440,6 +509,7 @@ QStringList KIO::DesktopExecParser::resultingArguments() const
         }
         terminal += QLatin1Char(' ') + d->service.terminalOptions();
         if (!mx1.expandMacrosShellQuote(terminal)) {
+            d->m_errorString = i18n("Syntax error in command %1 while trying to run %2", terminal, d->service.entryPath());
             qCWarning(KIO_CORE) << "Syntax error in command" << terminal << ", service" << d->service.name();
             return QStringList();
         }
@@ -450,20 +520,10 @@ QStringList KIO::DesktopExecParser::resultingArguments() const
 
     KShell::Errors err;
     QStringList execlist = KShell::splitArgs(exec, KShell::AbortOnMeta | KShell::TildeExpand, &err);
-    if (err == KShell::NoError && !execlist.isEmpty()) { // mx1 checked for syntax errors already
-        const QString executable = execlist.at(0);
-        if (QDir::isRelativePath(executable)) {
-            // Resolve the executable to ensure that helpers in libexec are found.
-            // Too bad for commands that need a shell - they must reside in $PATH.
-            QString exePath = QStandardPaths::findExecutable(executable);
-            if (exePath.isEmpty()) {
-                exePath = QFile::decodeName(CMAKE_INSTALL_FULL_LIBEXECDIR_KF5 "/") + executable;
-            }
-            if (QFile::exists(exePath)) {
-                execlist[0] = exePath;
-            }
-        }
+    if (!executableFullPath.isEmpty()) {
+        execlist[0] = executableFullPath;
     }
+
     if (d->service.substituteUid()) {
         if (d->service.terminal()) {
             result << QStringLiteral("su");
@@ -499,6 +559,11 @@ QStringList KIO::DesktopExecParser::resultingArguments() const
     return result;
 }
 
+QString KIO::DesktopExecParser::errorMessage() const
+{
+    return d->m_errorString;
+}
+
 //static
 QString KIO::DesktopExecParser::executableName(const QString &execLine)
 {
@@ -510,7 +575,7 @@ QString KIO::DesktopExecParser::executableName(const QString &execLine)
 QString KIO::DesktopExecParser::executablePath(const QString &execLine)
 {
     // Remove parameters and/or trailing spaces.
-    const QStringList args = KShell::splitArgs(execLine);
+    const QStringList args = KShell::splitArgs(execLine, KShell::AbortOnMeta | KShell::TildeExpand);
     for (const QString &arg : args) {
         if (!arg.contains(QLatin1Char('='))) {
             return arg;

src/core/desktopexecparser.h
@@ -79,8 +79,15 @@ public:
 
     /**
      * @return a list of arguments suitable for QProcess.
+     * Returns an empty list on error, check errorMessage() for details.
      */
     QStringList resultingArguments() const;
+
+    /**
+     * @return an error message for when resultingArguments() returns an empty list
+     * @since 5.71
+     */
+    QString errorMessage() const;
 
     /**
      * Returns the list of protocols which the application supports.

src/gui/kprocessrunner.cpp
@@ -46,37 +46,6 @@
 
 static int s_instanceCount = 0; // for the unittest
 
-static QString findNonExecutableProgram(const QString &executable)
-{
-    // Relative to current dir, or absolute path
-    const QFileInfo fi(executable);
-    if (fi.exists() && !fi.isExecutable()) {
-        return executable;
-    }
-
-#ifdef Q_OS_UNIX
-    // This is a *very* simplified version of QStandardPaths::findExecutable
-#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
-    const auto skipEmptyParts = QString::SkipEmptyParts;
-#else
-    const auto skipEmptyParts = Qt::SkipEmptyParts;
-#endif
-    const QStringList searchPaths = QString::fromLocal8Bit(qgetenv("PATH")).split(QDir::listSeparator(), skipEmptyParts);
-    for (const QString &searchPath : searchPaths) {
-        const QString candidate = searchPath + QLatin1Char('/') + executable;
-        const QFileInfo fileInfo(candidate);
-        if (fileInfo.exists()) {
-            if (fileInfo.isExecutable()) {
-                qWarning() << "Internal program error. QStandardPaths::findExecutable couldn't find" << executable << "but our own logic found it at" << candidate << ". Please report a bug at https://bugs.kde.org";
-            } else {
-                return candidate;
-            }
-        }
-    }
-#endif
-    return QString();
-}
-
 KProcessRunner::KProcessRunner(const KService::Ptr &service, const QList<QUrl> &urls,
                                KIO::ApplicationLauncherJob::RunFlags flags, const QString &suggestedFileName, const QByteArray &asn)
     : m_process{new KProcess},
@@ -94,20 +63,7 @@ KProcessRunner::KProcessRunner(const KService::Ptr &service, const QList<QUrl> &
     execParser.setSuggestedFileName(suggestedFileName);
     const QStringList args = execParser.resultingArguments();
     if (args.isEmpty()) {
-        emitDelayedError(i18n("Error processing Exec field in %1", service->entryPath()));
-        return;
-    }
-
-    const QString realExecutable = args.at(0);
-    // realExecutable is a full path if DesktopExecParser was able to locate it. Otherwise it's still relative, which is a bad sign.
-    if (QDir::isRelativePath(realExecutable) || !QFileInfo(realExecutable).isExecutable()) {
-        // Does it really not exist, or is it non-executable? (bug #415567)
-        const QString nonExecutable = findNonExecutableProgram(realExecutable);
-        if (nonExecutable.isEmpty()) {
-            emitDelayedError(i18n("Could not find the program '%1'", realExecutable));
-        } else {
-            emitDelayedError(i18n("The program '%1' was found at '%2' but it is missing executable permissions.", realExecutable, nonExecutable));
-        }
+        emitDelayedError(execParser.errorMessage());
         return;
     }
 

src/kioexec/main.cpp
@@ -206,6 +206,11 @@ void KIOExec::slotRunApp()
 
     KIO::DesktopExecParser execParser(service, list);
     QStringList params = execParser.resultingArguments();
+    if (params.isEmpty()) {
+        qWarning() << execParser.errorMessage();
+        QApplication::exit(-1);
+        return;
+    }
 
     qDebug() << "EXEC" << params.join(QLatin1Char(' '));
 