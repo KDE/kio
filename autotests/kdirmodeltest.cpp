KDE/kio

commit ebe7714300af53ba6f4b5cacddb53a2fd52c7d5a

Improve KDirModel to avoid showing '+' if there are no subdirs
Summary:
We either have the information at hand (in populated dirs)
or we query the filesystem (in non-populated dirs).

Test Plan: Tested by Raphael Rosch in D25315

Reviewers: apol, ahmadsamir, meven, rrosch

Reviewed By: rrosch

Subscribers: kde-frameworks-devel

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D27731

src/widgets/kdirmodel.cpp
@@ -32,6 +32,7 @@
 #include <QMimeData>
 #include <QBitArray>
 #include <QDebug>
+#include <QDirIterator>
 #include <QFile>
 #include <QFileInfo>
 #include <QDir>
@@ -1144,9 +1145,21 @@ bool KDirModel::hasChildren(const QModelIndex &parent) const
         return true;
     }
 
-    const KFileItem &parentItem = static_cast<KDirModelNode *>(parent.internalPointer())->item();
+    const KDirModelNode *parentNode = static_cast<KDirModelNode *>(parent.internalPointer());
+    const KFileItem &parentItem = parentNode->item();
     Q_ASSERT(!parentItem.isNull());
-    return parentItem.isDir();
+    if (!parentItem.isDir()) {
+        return false;
+    }
+    if (static_cast<const KDirModelDirNode *>(parentNode)->isPopulated()) {
+        return !static_cast<const KDirModelDirNode *>(parentNode)->m_childNodes.isEmpty();
+    }
+    if (parentItem.isLocalFile()) {
+        QDirIterator it(parentItem.localPath(), QDir::Dirs | QDir::NoSymLinks | QDir::NoDotAndDotDot,  QDirIterator::Subdirectories);
+        return it.hasNext();
+    }
+    // Remote and not listed yet, we can't know; let the user click on it so we'll find out
+    return true;
 }
 
 Qt::ItemFlags KDirModel::flags(const QModelIndex &index) const

commit fe87d93250d2f9b302e0498cc99a2b7d9fab7583

KDirModel: fix hasChildren() regression for trees with files shown
The logic I introduced in commit ebe7714300af53ba6f4b5cacddb53a2fd52c7d5a
assumed that only subdirs were shown in the tree (konqueror sidebar use
case). However KDirOperator can also show files, so this introduced bug
419434. Now implemented correctly for both use cases, with unittest.

BUG: 419434
FIXED-IN: 5.71

autotests/kdirmodeltest.cpp
@@ -284,6 +284,7 @@ void KDirModelTest::testIndex()
     QModelIndex sibling1 = m_dirModel->sibling(subsubdirIndex.row(), 0, m_fileInDirIndex);
     QVERIFY(sibling1.isValid());
     QVERIFY(sibling1 == subsubdirIndex);
+    QVERIFY(m_dirModel->hasChildren(subsubdirIndex));
 
     // Invalid sibling call
     QVERIFY(!m_dirModel->sibling(1, 0, m_fileInSubdirIndex).isValid());
@@ -1239,6 +1240,51 @@ void KDirModelTest::testShowRootAndExpandToUrl()
     const QUrl homeUrl = QUrl::fromLocalFile(QDir::homePath());
     dirModel.expandToUrl(homeUrl);
     QTRY_VERIFY(dirModel.indexForUrl(homeUrl).isValid());
+}
+
+void KDirModelTest::testHasChildren_data()
+{
+    QTest::addColumn<bool>("dirsOnly");
+
+    QTest::newRow("with_files") << false;
+    QTest::newRow("dirs_only") << true;
+}
+
+// Test hasChildren without first populating the dirs
+void KDirModelTest::testHasChildren()
+{
+    QFETCH(bool, dirsOnly);
+
+    m_dirModel->dirLister()->setDirOnlyMode(dirsOnly);
+    fillModel(true, false);
+
+    QVERIFY(m_dirModel->hasChildren());
+
+    auto firstSubdirIndex = [this](const QModelIndex &parentIndex) {
+        for (int row = 0; row < m_dirModel->rowCount(parentIndex); ++row) {
+            QModelIndex idx = m_dirModel->index(row, 0, parentIndex);
+            if (m_dirModel->itemForIndex(idx).isDir()) {
+                return idx;
+            }
+        }
+        return QModelIndex();
+    };
+
+    m_dirIndex = firstSubdirIndex(QModelIndex());
+    QVERIFY(m_dirIndex.isValid());
+    QVERIFY(m_dirModel->hasChildren(m_dirIndex));
+
+    auto listDir = [this](const QModelIndex &index) {
+        QSignalSpy completedSpy(m_dirModel->dirLister(), QOverload<>::of(&KDirLister::completed));
+        m_dirModel->fetchMore(index);
+        return completedSpy.wait();
+    };
+    // Now list subdir/
+    QVERIFY(listDir(m_dirIndex));
+
+    const QModelIndex subsubdirIndex = firstSubdirIndex(m_dirIndex);
+    QVERIFY(subsubdirIndex.isValid());
+    QCOMPARE(m_dirModel->hasChildren(subsubdirIndex), !dirsOnly);
 }
 
 void KDirModelTest::testDeleteFile()

autotests/kdirmodeltest.h
@@ -65,6 +65,8 @@ private Q_SLOTS:
     void testShowRoot();
     void testShowRootWithTrailingSlash();
     void testShowRootAndExpandToUrl();
+    void testHasChildren_data();
+    void testHasChildren();
 
     // These tests must be done last
     void testDeleteFile();

src/widgets/kdirmodel.cpp
@@ -1216,7 +1216,11 @@ bool KDirModel::hasChildren(const QModelIndex &parent) const
         return !static_cast<const KDirModelDirNode *>(parentNode)->m_childNodes.isEmpty();
     }
     if (parentItem.isLocalFile()) {
-        QDirIterator it(parentItem.localPath(), QDir::Dirs | QDir::NoSymLinks | QDir::NoDotAndDotDot,  QDirIterator::Subdirectories);
+        QDir::Filters filters = QDir::Dirs | QDir::NoSymLinks | QDir::NoDotAndDotDot;
+        if (!d->m_dirLister->dirOnlyMode()) {
+            filters |= QDir::Files;
+        }
+        QDirIterator it(parentItem.localPath(), filters,  QDirIterator::Subdirectories);
         return it.hasNext();
     }
     // Remote and not listed yet, we can't know; let the user click on it so we'll find out