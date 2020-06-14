7cec5805d0751b44e7778c327a19a5ed4590adae
Adriaan de Groot <groot@kde.org>
2020-06-14 15:45:26 +0200

    Start renaming KIO Slaves to KIO Workers.

---
+++ src/core/workerbase.h
@@ -0,0 +1,20 @@
+/*
+ * 
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * License-Filename: COPYING.LIB
+ * 
+ */
+
+#ifndef KIO_WORKERBASE_H
+#define KIO_WORKERBASE_H
+
+#include "slavebase.h"
+
+namespace KIO
+{
+    
+    using WorkerBase = SlaveBase;
+    
+}
+
+#endif
