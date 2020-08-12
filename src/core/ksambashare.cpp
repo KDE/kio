1febcc976c954105048a98ca3a13cacb93a8a965
Harald Sitter <sitter@kde.org>
2020-08-12 15:29:10 +0200

    smb: fix share name availability check
    s/||/&&
    
    a share name must not be a user name
    AND
    a share name must not be used by an existing share already

--- src/core/ksambashare.cpp
+++ src/core/ksambashare.cpp
@@ -274,7 +274,7 @@ bool KSambaSharePrivate::isDirectoryShared(const QString &path) const
 bool KSambaSharePrivate::isShareNameAvailable(const QString &name) const
 {
     // Samba does not allow to name a share with a user name registered in the system
-    return (!KUser::allUserNames().contains(name) || !data.contains(name));
+    return (!KUser::allUserNames().contains(name) && !data.contains(name));
 }
 
 KSambaShareData::UserShareError KSambaSharePrivate::isPathValid(const QString &path) const
