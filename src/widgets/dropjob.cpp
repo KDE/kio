6d80ecfe4de8dd2b7aa8f17bc0fb755cead6811b
David Redondo <kde@david-redondo.de>
2020-11-11 13:36:47 +0100

    DropMenu: Use translated shortcuts
    I noticed that it shows "Ctrl" instead of localized "Strg" on my system.

--- src/widgets/dropjob.cpp
+++ src/widgets/dropjob.cpp
@@ -163,7 +163,7 @@ DropMenu::DropMenu(QWidget *parent)
     : QMenu(parent),
       m_extraActionsSeparator(nullptr)
 {
-    m_cancelAction = new QAction(i18n("C&ancel") + QLatin1Char('\t') + QKeySequence(Qt::Key_Escape).toString(), this);
+    m_cancelAction = new QAction(i18n("C&ancel") + QLatin1Char('\t') + QKeySequence(Qt::Key_Escape).toString(QKeySequence::NativeText), this);
     m_cancelAction->setIcon(QIcon::fromTheme(QStringLiteral("process-stop")));
 
     m_lastSeparator = new QAction(this);
@@ -330,21 +330,19 @@ void DropJobPrivate::fillPopupMenu(KIO::DropMenu *popup)
     const bool sDeleting = itemProps.supportsDeleting();
     const bool sMoving = itemProps.supportsMoving();
 
-    QString seq = QKeySequence(Qt::ShiftModifier).toString();
-    Q_ASSERT(seq.endsWith(QLatin1Char('+')));
-    seq.chop(1); // chop superfluous '+'
+    const int separatorLength = QCoreApplication::translate("QShortcut", "+").size();
+    QString seq = QKeySequence(Qt::ShiftModifier).toString(QKeySequence::NativeText);
+    seq.chop(separatorLength); // chop superfluous '+'
     QAction* popupMoveAction = new QAction(i18n("&Move Here") + QLatin1Char('\t') + seq, popup);
     popupMoveAction->setIcon(QIcon::fromTheme(QStringLiteral("edit-move"), QIcon::fromTheme(QStringLiteral("go-jump"))));
     popupMoveAction->setData(QVariant::fromValue(Qt::MoveAction));
-    seq = QKeySequence(Qt::ControlModifier).toString();
-    Q_ASSERT(seq.endsWith(QLatin1Char('+')));
-    seq.chop(1);
+    seq = QKeySequence(Qt::ControlModifier).toString(QKeySequence::NativeText);
+    seq.chop(separatorLength);
     QAction* popupCopyAction = new QAction(i18n("&Copy Here") + QLatin1Char('\t') + seq, popup);
     popupCopyAction->setIcon(QIcon::fromTheme(QStringLiteral("edit-copy")));
     popupCopyAction->setData(QVariant::fromValue(Qt::CopyAction));
-    seq = QKeySequence(Qt::ControlModifier + Qt::ShiftModifier).toString();
-    Q_ASSERT(seq.endsWith(QLatin1Char('+')));
-    seq.chop(1);
+    seq = QKeySequence(Qt::ControlModifier + Qt::ShiftModifier).toString(QKeySequence::NativeText);
+    seq.chop(separatorLength);
     QAction* popupLinkAction = new QAction(i18n("&Link Here") + QLatin1Char('\t') + seq, popup);
     popupLinkAction->setIcon(QIcon::fromTheme(QStringLiteral("edit-link")));
     popupLinkAction->setData(QVariant::fromValue(Qt::LinkAction));
