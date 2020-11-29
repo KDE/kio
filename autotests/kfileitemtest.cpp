KDE/kio

commit eb20176d1a42eebd4c6c3cbdb5dca6a854760736

KIO::iconNameForUrl: fix searching for kde protocol icons
Summary:
iconName was set to mt.iconName() at the beginning, but this returns always "application-octet-stream" making all subsequent iconName.isEmpty() check not work as expected.
This patch sets iconName to mt.iconName() only when needed.

BUG: 417922
BUG: 417921
CCBUG: 417069
FIXED-IN: 5.68

Test Plan:
In dolphin:
 - open "Recent Files"
 - Ctrl+T

 - open any folder
 - Ctrl+T
 - Ctrl+F (Search
 - Search for anything, type something then return

Same for smb:/ location

In all cases the protocol icon is now visible.

Reviewers: ngraham, #frameworks, dfaure, broulik, sitter

Reviewed By: ngraham, dfaure, sitter

Subscribers: sitter, kde-frameworks-devel

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D27539

autotests/kfileitemtest.cpp
@@ -30,6 +30,7 @@
 #include "kiotesthelper.h"
 
 #include <QMimeDatabase>
+#include <KProtocolInfo>
 
 QTEST_MAIN(KFileItemTest)
 
@@ -620,7 +621,18 @@ void KFileItemTest::testIconNameForUrl_data()
     QTest::newRow("relative") << QUrl("foo") << "unknown";
     QTest::newRow("tilde") << QUrl("~") << "unknown";
 
-    // TODO more tests
+    QTest::newRow("unknownscheme folder") << QUrl("unknownscheme:/") << "inode-directory";
+    QTest::newRow("unknownscheme file") << QUrl("unknownscheme:/test") << "application-octet-stream";
+
+    QTest::newRow("trash folder") << QUrl("trash:/") << "user-trash-full";
+    QTest::newRow("trash file") << QUrl("trash:/test") << "user-trash-full";
+
+    QTest::newRow("https scheme") << QUrl("https://kde.org/") << "text-html";
+
+    if (KProtocolInfo::isKnownProtocol("smb")) {
+        QTest::newRow("smb folder") << QUrl("smb:/") << "network-workgroup";
+        QTest::newRow("smb file") << QUrl("smb:/test") << "network-workgroup";
+    }
 }
 
 void KFileItemTest::testIconNameForUrl()

src/core/global.cpp
@@ -223,25 +223,23 @@ QString KIO::favIconForUrl(const QUrl &url)
 
 QString KIO::iconNameForUrl(const QUrl &url)
 {
-    const QLatin1String unknown("unknown");
     if (url.scheme().isEmpty()) { // empty URL or relative URL (e.g. '~')
-        return unknown;
+        return QStringLiteral("unknown");
     }
     QMimeDatabase db;
     const QMimeType mt = db.mimeTypeForUrl(url);
-    const QString mimeTypeIcon = mt.iconName();
-    QString i = mimeTypeIcon;
+    QString iconName;
 
     if (url.isLocalFile()) {
         // Check to see whether it's an xdg location (e.g. Pictures folder)
         if (mt.inherits(QStringLiteral("inode/directory"))) {
-            i = KIOPrivate::iconForStandardPath(url.toLocalFile());
+            iconName = KIOPrivate::iconForStandardPath(url.toLocalFile());
         }
 
         // Let KFileItem::iconName handle things for us
-        if (i == unknown || i.isEmpty() || mt.isDefault()) {
+        if (iconName.isEmpty()) {
             const KFileItem item(url, mt.name());
-            i = item.iconName();
+            iconName = item.iconName();
         }
 
     } else {
@@ -249,29 +247,27 @@ QString KIO::iconNameForUrl(const QUrl &url)
 
         // Look for a favicon
         if (url.scheme().startsWith(QLatin1String("http"))) {
-            i = favIconForUrl(url);
+            iconName = favIconForUrl(url);
         }
 
         // Then handle the trash
         else if (url.scheme() == QLatin1String("trash") && url.path().length() <= 1) {
             KConfig trashConfig(QStringLiteral("trashrc"), KConfig::SimpleConfig);
             if (trashConfig.group("Status").readEntry("Empty", true)) {
-                i = QStringLiteral("user-trash");
+                iconName = QStringLiteral("user-trash");
             } else {
-                i = QStringLiteral("user-trash-full");
+                iconName = QStringLiteral("user-trash-full");
             }
         }
 
-        if (i.isEmpty()) {
-            i = KProtocolInfo::icon(url.scheme());
-        }
-
-        // root of protocol: if we found nothing, revert to mimeTypeIcon (which is usually "folder")
-        if (url.path().length() <= 1 && (i == unknown || i.isEmpty())) {
-            i = mimeTypeIcon;
+        // and other protocols
+        if (iconName.isEmpty()) {
+            iconName = KProtocolInfo::icon(url.scheme());
         }
     }
-    return !i.isEmpty() ? i : unknown;
+    // if we found nothing, return QMimeType.iconName()
+    // (which fallbacks to "application-octet-stream" when no mimetype could be determined)
+    return !iconName.isEmpty() ? iconName : mt.iconName();
 }
 
 QUrl KIO::upUrl(const QUrl &url)

commit 55465cc6639b7e5d83a9cdf30a010e0b677765c3

KIO::iconNameForUrl: resolve icon for remote files based on name
Fix a regression introduced with eb20176d1a42eebd4c6c3cbdb5dca6a854760736

BUG: 429530
FIXED-IN: 5.77

autotests/kfileitemtest.cpp
@@ -615,12 +615,15 @@ void KFileItemTest::testIconNameForUrl_data()
     QTest::newRow("trash:/ itself") << QUrl("trash:/") << "user-trash-full";
     QTest::newRow("folder under trash:/") << QUrl("trash:/folder/") << "inode-directory";
     QTest::newRow("file under trash:/") << QUrl("trash:/test") << "application-octet-stream";
+    QTest::newRow("image file under trash:/") << QUrl("trash:/test.png") << "image-png";
 
     QTest::newRow("https scheme") << QUrl("https://kde.org/") << "text-html";
 
     if (KProtocolInfo::isKnownProtocol("smb")) {
-        QTest::newRow("smb folder") << QUrl("smb:/") << "network-workgroup";
-        QTest::newRow("smb file") << QUrl("smb:/test") << "network-workgroup";
+        QTest::newRow("smb root") << QUrl("smb:/") << "network-workgroup";
+        QTest::newRow("smb unknown file") << QUrl("smb:/test") << "network-workgroup";
+        QTest::newRow("smb directory/") << QUrl("smb:/unknown/") << "inode-directory";
+        QTest::newRow("smb image file") << QUrl("smb:/test.png") << "image-png";
     }
 }
 

src/core/global.cpp
@@ -253,7 +253,7 @@ QString KIO::iconNameForUrl(const QUrl &url)
         }
 
         // and other protocols
-        if (iconName.isEmpty()) {
+        if (iconName.isEmpty() && (mt.isDefault() || url.path().size() <= 1)) {
             iconName = KProtocolInfo::icon(url.scheme());
         }
     }