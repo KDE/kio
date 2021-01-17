cfa6b12c9ed822410bd39cc7e0794906bd180bf0
Kai Uwe Broulik <kde@privat.broulik.de>
2021-01-17 19:12:46 +0100

    [KDynamicJobTracker] Support KUiServerV2JobTracker
    Track the KUIServer DBus service and check (unfortunately through DBus
    introspection) whether it supports API version 2 and cache it until the
    service owner changes or disappears. This way only blocking DBus call
    is only done initially and subsequent jobs are block-free.
    
    KUiServer is also part of Plasma these days, so there's no need for asking it
    about whether a job view is present. This cannot really be changed for V1
    but for V2 we can skip this.

--- src/widgets/kdynamicjobtracker.cpp
+++ src/widgets/kdynamicjobtracker.cpp
@@ -1,6 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2008 Rob Scheepmaker <r.scheepmaker@student.utwente.nl>
     SPDX-FileCopyrightText: 2010 Shaun Reich <shaun.reich@kdemail.net>
+    SPDX-FileCopyrightText: 2021 Kai Uwe Broulik <kde@broulik.de>
 
     SPDX-License-Identifier: LGPL-2.1-or-later
 */
@@ -11,16 +12,20 @@
 
 #include <KJobTrackerInterface>
 #include <KUiServerJobTracker>
+#include <KUiServerV2JobTracker>
 #include <KWidgetJobTracker>
 #include <kio/jobtracker.h>
 
 #include <QApplication>
 #include <QDBusConnection>
 #include <QDBusConnectionInterface>
+#include <QDBusServiceWatcher>
 #include <QMap>
+#include <QXmlStreamReader>
 
 struct AllTrackers {
     KUiServerJobTracker *kuiserverTracker;
+    KUiServerV2JobTracker *kuiserverV2Tracker;
     KWidgetJobTracker *widgetTracker;
 };
 
@@ -34,12 +39,40 @@ public:
     ~KDynamicJobTrackerPrivate()
     {
         delete kuiserverTracker;
+        delete kuiserverV2Tracker;
         delete widgetTracker;
     }
 
+    static bool hasDBusInterface(const QString &introspectionData, const QString &interface)
+    {
+        QXmlStreamReader xml(introspectionData);
+        while (!xml.atEnd() && !xml.hasError()) {
+            xml.readNext();
+
+            if (xml.tokenType() == QXmlStreamReader::StartElement
+                    && xml.name() == QLatin1String("interface")) {
+
+                if (xml.attributes().value(QLatin1String("name")) == interface) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     KUiServerJobTracker *kuiserverTracker = nullptr;
+    KUiServerV2JobTracker *kuiserverV2Tracker = nullptr;
     KWidgetJobTracker *widgetTracker = nullptr;
     QMap<KJob *, AllTrackers> trackers;
+
+    enum JobViewServerSupport {
+        NeedsChecking,
+        Error,
+        V2Supported,
+        V2NotSupported
+    };
+    JobViewServerSupport jobViewServerSupport = NeedsChecking;
+    QDBusServiceWatcher *jobViewServerWatcher = nullptr;
 };
 
 KDynamicJobTracker::KDynamicJobTracker(QObject *parent)
@@ -65,6 +98,9 @@ void KDynamicJobTracker::registerJob(KJob *job)
     // always add an entry, even with no trackers used at all,
     // so unregisterJob() will work as normal
     AllTrackers &trackers = d->trackers[job];
+    trackers.kuiserverTracker = nullptr;
+    trackers.kuiserverV2Tracker = nullptr;
+    trackers.widgetTracker = nullptr;
 
     // do not try to query kuiserver if dbus is not available
     if (!QDBusConnection::sessionBus().interface()) {
@@ -76,33 +112,83 @@ void KDynamicJobTracker::registerJob(KJob *job)
 
             trackers.widgetTracker = d->widgetTracker;
             trackers.widgetTracker->registerJob(job);
+        }
+        return;
+    }
+
+    const QString kuiserverService = QStringLiteral("org.kde.kuiserver");
+
+    if (!d->jobViewServerWatcher) {
+        d->jobViewServerWatcher = new QDBusServiceWatcher(kuiserverService,
+                                                          QDBusConnection::sessionBus(),
+                                                          QDBusServiceWatcher::WatchForOwnerChange | QDBusServiceWatcher::WatchForUnregistration,
+                                                          this);
+        connect(d->jobViewServerWatcher, &QDBusServiceWatcher::serviceOwnerChanged, this, [this] {
+            d->jobViewServerSupport = KDynamicJobTrackerPrivate::NeedsChecking;
+        });
+    }
+
+    if (d->jobViewServerSupport == KDynamicJobTrackerPrivate::NeedsChecking) {
+        // Unfortunately no DBus ObjectManager support in Qt DBus.
+        QDBusMessage msg = QDBusMessage::createMethodCall(kuiserverService,
+                                                          QStringLiteral("/JobViewServer"),
+                                                          QStringLiteral("org.freedesktop.DBus.Introspectable"),
+                                                          QStringLiteral("Introspect"));
+        auto reply = QDBusConnection::sessionBus().call(msg);
+        if (reply.type() == QDBusMessage::ErrorMessage || reply.arguments().count() != 1)  {
+            qCWarning(KIO_WIDGETS) << "Failed to check which JobView API is supported" << reply.errorMessage();
+            d->jobViewServerSupport = KDynamicJobTrackerPrivate::Error;
         } else {
-            trackers.widgetTracker = nullptr;
+            const QString introspectionData = reply.arguments().first().toString();
+
+            if (KDynamicJobTrackerPrivate::hasDBusInterface(introspectionData, QStringLiteral("org.kde.JobViewServerV2"))) {
+                d->jobViewServerSupport = KDynamicJobTrackerPrivate::V2Supported;
+            } else {
+                d->jobViewServerSupport = KDynamicJobTrackerPrivate::V2NotSupported;
+            }
+        }
+    }
+
+    if (d->jobViewServerSupport == KDynamicJobTrackerPrivate::V2Supported) {
+        if (!d->kuiserverV2Tracker) {
+            d->kuiserverV2Tracker = new KUiServerV2JobTracker();
         }
 
-        trackers.kuiserverTracker = nullptr;
-    } else {
+        trackers.kuiserverV2Tracker = d->kuiserverV2Tracker;
+        trackers.kuiserverV2Tracker->registerJob(job);
+        return;
+    }
+
+    // No point in trying to set up V1 if calling the service above failed.
+    if (d->jobViewServerSupport != KDynamicJobTrackerPrivate::Error) {
         if (!d->kuiserverTracker) {
             d->kuiserverTracker = new KUiServerJobTracker();
         }
 
         trackers.kuiserverTracker = d->kuiserverTracker;
         trackers.kuiserverTracker->registerJob(job);
+    }
 
-        trackers.widgetTracker = nullptr;
-        if (canHaveWidgets) {
-            org::kde::kuiserver interface(QStringLiteral("org.kde.kuiserver"), QStringLiteral("/JobViewServer"), QDBusConnection::sessionBus(), this);
+    if (canHaveWidgets) {
+        bool needsWidgetTracker = d->jobViewServerSupport == KDynamicJobTrackerPrivate::Error;
+
+        if (!needsWidgetTracker) {
+            org::kde::kuiserver interface(kuiserverService,
+                                          QStringLiteral("/JobViewServer"),
+                                          QDBusConnection::sessionBus(),
+                                          this);
             QDBusReply<bool> reply = interface.requiresJobTracker();
+            needsWidgetTracker = !reply.isValid() || reply.value();
+        }
 
-            // If kuiserver isn't available or it tells us a job tracker is required
-            // create a widget tracker.
-            if (!reply.isValid() || reply.value()) {
-                if (!d->widgetTracker) {
-                    d->widgetTracker = new KWidgetJobTracker();
-                }
-                trackers.widgetTracker = d->widgetTracker;
-                trackers.widgetTracker->registerJob(job);
+        // If kuiserver isn't available or it tells us a job tracker is required
+        // create a widget tracker.
+        if (needsWidgetTracker) {
+            if (!d->widgetTracker) {
+                d->widgetTracker = new KWidgetJobTracker();
             }
+            trackers.widgetTracker = d->widgetTracker;
+            trackers.widgetTracker->registerJob(job);
         }
     }
 }
@@ -120,10 +206,15 @@ void KDynamicJobTracker::unregisterJob(KJob *job)
 
     const AllTrackers &trackers = it.value();
     KUiServerJobTracker *kuiserverTracker = trackers.kuiserverTracker;
+    KUiServerV2JobTracker *kuiserverV2Tracker = trackers.kuiserverV2Tracker;
     KWidgetJobTracker *widgetTracker = trackers.widgetTracker;
 
     if (kuiserverTracker) {
         kuiserverTracker->unregisterJob(job);
+    }
+
+    if (kuiserverV2Tracker) {
+        kuiserverV2Tracker->unregisterJob(job);
     }
 
     if (widgetTracker) {
