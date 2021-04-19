KDE/kio

commit a439211c1f982624453407a9fef4551e9e51360a

Deprecate the remnants of FSDevice-related code
The "link to device" template .desktop file hadn't been installed for a long
time; see commit d1018260814a6e7 and [1] for more details.

[1] https://invent.kde.org/frameworks/kio/-/merge_requests/409

src/widgets/kdesktopfileactions.h
@@ -22,12 +22,18 @@ class KService;
  */
 namespace KDesktopFileActions
 {
+#if KIOWIDGETS_ENABLE_DEPRECATED_SINCE(5, 82)
 /**
  * Returns a list of services for the given .desktop file that are handled
- * by kio itself. Namely mount/unmount for FSDevice files.
+ * by KIO itself. Namely mount/unmount for FSDevice files.
  * @return the list of services
+ *
+ * @deprecated since 5.82, FSDevice .desktop file template hasn't been installed
+ * for a long time. And users don't seem to mount devices using that method any more.
  */
+KIOWIDGETS_DEPRECATED_VERSION(5, 82, "For lack of usage.");
 KIOWIDGETS_EXPORT QList<KServiceAction> builtinServices(const QUrl &url);
+#endif
 
 /**
  * Returns a list of services defined by the user as possible actions

src/core/kfileitem.cpp
@@ -1092,6 +1092,7 @@ QStringList KFileItem::overlays() const
             names.append(QStringLiteral("emblem-important"));
         }
 
+#if KIOCORE_BUILD_DEPRECATED_SINCE(5, 82)
         if (cfg.hasDeviceType()) {
             const QString dev = cfg.readDevice();
             if (!dev.isEmpty()) {
@@ -1101,6 +1102,7 @@ QStringList KFileItem::overlays() const
                 }
             }
         }
+#endif
     }
 
     if (isHidden()) {

src/widgets/CMakeLists.txt
@@ -100,7 +100,7 @@ ecm_generate_export_header(KF5KIOWidgets
     GROUP_BASE_NAME KF
     VERSION ${KF_VERSION}
     DEPRECATED_BASE_VERSION 0
-    DEPRECATION_VERSIONS 4.0 4.1 4.3 4.4 4.5 4.6 4.7 5.0 5.4 5.6 5.25 5.31 5.32 5.64 5.66 5.71 5.75 5.76 5.79 5.80
+    DEPRECATION_VERSIONS 4.0 4.1 4.3 4.4 4.5 4.6 4.7 5.0 5.4 5.6 5.25 5.31 5.32 5.64 5.66 5.71 5.75 5.76 5.79 5.80 5.82
     EXCLUDE_DEPRECATED_BEFORE_AND_AT ${EXCLUDE_DEPRECATED_BEFORE_AND_AT}
 )
 # TODO: add support for EXCLUDE_DEPRECATED_BEFORE_AND_AT to all KIO libs

src/widgets/kdesktopfileactions.cpp
@@ -142,6 +142,7 @@ static bool runLink(const QUrl &_url, const KDesktopFile &cfg, const QByteArray 
 }
 #endif
 
+#if KIOWIDGETS_BUILD_DEPRECATED_SINCE(5, 82)
 QList<KServiceAction> KDesktopFileActions::builtinServices(const QUrl &_url)
 {
     QList<KServiceAction> result;
@@ -193,6 +194,7 @@ QList<KServiceAction> KDesktopFileActions::builtinServices(const QUrl &_url)
 
     return result;
 }
+#endif
 
 QList<KServiceAction> KDesktopFileActions::userDefinedServices(const QString &path, bool bLocalFiles)
 {

src/widgets/kfileitemactions.cpp
@@ -81,9 +81,17 @@ class PopupServices
 public:
     ServiceList &selectList(const QString &priority, const QString &submenuName);
 
+#if KIOWIDGETS_BUILD_DEPRECATED_SINCE(5, 82)
     ServiceList builtin;
-    ServiceList user, userToplevel, userPriority;
-    QMap<QString, ServiceList> userSubmenus, userToplevelSubmenus, userPrioritySubmenus;
+#endif
+
+    ServiceList user;
+    ServiceList userToplevel;
+    ServiceList userPriority;
+
+    QMap<QString, ServiceList> userSubmenus;
+    QMap<QString, ServiceList> userToplevelSubmenus;
+    QMap<QString, ServiceList> userPrioritySubmenus;
 };
 
 ServiceList &PopupServices::selectList(const QString &priority, const QString &submenuName)
@@ -639,11 +647,14 @@ QPair<int, QMenu *> KFileItemActionsPrivate::addServiceActionsTo(QMenu *mainMenu
 
     KIO::PopupServices s;
 
+    // TODO KF6 remove mention of "builtin" (deprecated)
     // 1 - Look for builtin and user-defined services
     if (isSingleLocal && m_props.mimeType() == QLatin1String("application/x-desktop")) {
-        // get builtin services, like mount/unmount
+#if KIOWIDGETS_BUILD_DEPRECATED_SINCE(5, 82)
+        // Get builtin services, like mount/unmount
+        s.builtin = KDesktopFileActions::builtinServices(QUrl::fromLocalFile(firstItem.localPath()));
+#endif
         const QString path = firstItem.localPath();
-        s.builtin = KDesktopFileActions::builtinServices(QUrl::fromLocalFile(path));
         const KDesktopFile desktopFile(path);
         const KConfigGroup cfg = desktopFile.desktopGroup();
         const QString priority = cfg.readEntry("X-KDE-Priority");
@@ -719,7 +730,11 @@ QPair<int, QMenu *> KFileItemActionsPrivate::addServiceActionsTo(QMenu *mainMenu
     userItemCount += insertServices(s.userPriority, actionMenu, false);
     userItemCount += insertServicesSubmenus(s.userSubmenus, actionMenu, false);
     userItemCount += insertServices(s.user, actionMenu, false);
+
+#if KIOWIDGETS_BUILD_DEPRECATED_SINCE(5, 82)
     userItemCount += insertServices(s.builtin, mainMenu, true);
+#endif
+
     userItemCount += insertServicesSubmenus(s.userToplevelSubmenus, mainMenu, false);
     userItemCount += insertServices(s.userToplevel, mainMenu, false);
 

src/widgets/kfileitemactions.h
@@ -24,12 +24,11 @@ class KFileItemActionsPrivate;
  *
  * This includes:
  * @li "open with <application>" actions, but also
- * @li builtin services like mount/unmount for old-style device desktop files
  * @li user-defined actions for a .desktop file, defined in the file itself (see the desktop entry standard)
  * @li servicemenus actions, defined in .desktop files and selected based on the MIME type of the url
  *
  * KFileItemActions respects Kiosk-based restrictions (see the KAuthorized
- * namespace in the KConfig framework).  In particular, the "action/openwith"
+ * namespace in the KConfig framework). In particular, the "action/openwith"
  * action is checked when determining actions for opening files (see
  * addOpenWithActionsTo()) and service-specific actions are checked before
  * adding service actions to a menu (see addServiceActionsTo()).
@@ -38,6 +37,11 @@ class KFileItemActionsPrivate;
  * can be used to determine which actions are checked before the user-defined
  * action is allowed.  The action is ignored if any of the listed actions are
  * not authorized.
+ *
+ * @note: The builtin services like mount/unmount for old-style device desktop
+ * files (which mainly concerns CDROM and Floppy drives) have been deprecated
+ * since 5.82; those menu entries were hidden long before that, since the FSDevice
+ * .desktop template file hadn't been installed for quite a while.
  *
  * @since 4.3
  */
@@ -187,8 +191,9 @@ public:
 #endif
 
     enum class MenuActionSource {
-        Services = 0x1, // Add builtin actions, user defined actions and servicemenu actions
-        Plugins = 0x2, // Add actions implemented by plugins. See KAbstractFileItemActionPlugin base class.
+        Services = 0x1, ///< Add user defined actions and servicemenu actions (this used to include builtin
+                        ///< actions, which have been deprecated since 5.82 see class API documentation)
+        Plugins = 0x2, ///< Add actions implemented by plugins. See KAbstractFileItemActionPlugin base class.
         All = Services | Plugins,
     };
     Q_DECLARE_FLAGS(MenuActionSources, MenuActionSource)

commit c4e422d990dc682a587fa53dcd73188110f57ddc

Fix compile error introduced by a439211c1f9826
With semicolon added it fails with,

/opt/kde/include/KF5/KIOWidgets/kdesktopfileactions.h:34:1: error: declaration does not declare anything [-fpermissive]
   34 | KIOWIDGETS_DEPRECATED_VERSION(5, 82, "For lack of usage.");

src/widgets/kdesktopfileactions.h
@@ -31,7 +31,7 @@ namespace KDesktopFileActions
  * @deprecated since 5.82, FSDevice .desktop file template hasn't been installed
  * for a long time. And users don't seem to mount devices using that method any more.
  */
-KIOWIDGETS_DEPRECATED_VERSION(5, 82, "For lack of usage.");
+KIOWIDGETS_DEPRECATED_VERSION(5, 82, "For lack of usage.")
 KIOWIDGETS_EXPORT QList<KServiceAction> builtinServices(const QUrl &url);
 #endif
 