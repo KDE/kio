5fa55a2395cbfb6504e56bf71c869c8e49902e13
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-07-03 09:18:38 +0200

    Allow using scheme handler in OpenURLJob if kio slave exists too
    In case of https://invent.kde.org/system/khelpcenter/-/merge_requests/8
    the scheme handler gets defined for the app, but there is also a KIO
    slave which handles the same URL. Considering that most apps use the XDG
    mechanism to handle URLs instead of providing an ioslave with a protocol
    file we should not enforce using the ioslaves.

--- src/core/desktopexecparser.cpp
+++ src/core/desktopexecparser.cpp
@@ -219,20 +219,10 @@ bool KIO::DesktopExecParser::isProtocolInSupportedList(const QUrl &url, const QS
 // We have up to two sources of data, for protocols not handled by kioslaves (so called "helper") :
 // 1) the exec line of the .protocol file, if there's one
 // 2) the application associated with x-scheme-handler/<protocol> if there's one
-
-// If both exist, then:
-//  A) if the .protocol file says "launch an application", then the new-style handler-app has priority
-//  B) but if the .protocol file is for a kioslave (e.g. kio_http) then this has priority over
-//     firefox or chromium saying x-scheme-handler/http. Gnome people want to send all HTTP urls
-//     to a webbrowser, but we want MIME-type-determination-in-calling-application by default
-//     (the user can configure a BrowserApplication though)
 bool KIO::DesktopExecParser::hasSchemeHandler(const QUrl &url) // KF6 TODO move to OpenUrlJob
 {
     if (KProtocolInfo::isHelperProtocol(url)) {
         return true;
-    }
-    if (KProtocolInfo::isKnownProtocol(url)) {
-        return false; // this is case B, we prefer kioslaves over the competition
     }
     const KService::Ptr service = KApplicationTrader::preferredService(QLatin1String("x-scheme-handler/") + url.scheme());
     if (service) {
