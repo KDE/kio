2602cd50084f595c755127358a8c104eba90480c
Nicolas Fella <nicolas.fella@gmx.de>
2021-12-14 20:09:48 +0100

    Remove calls to no-op KNewFileMenu::setViewShowsHiddenFiles
    The implementation doesn't do anything

--- src/filewidgets/kdiroperator.cpp
+++ src/filewidgets/kdiroperator.cpp
@@ -699,7 +699,6 @@ void KDirOperatorPrivate::slotToggleIgnoreCase()
 void KDirOperator::mkdir()
 {
     d->m_newFileMenu->setPopupFiles(QList<QUrl>{url()});
-    d->m_newFileMenu->setViewShowsHiddenFiles(showHiddenFiles());
     d->m_newFileMenu->createDirectory();
 }
 
@@ -1301,7 +1300,6 @@ void KDirOperator::activatedMenu(const KFileItem &item, const QPoint &pos)
     updateSelectionDependentActions();
 
     d->m_newFileMenu->setPopupFiles(QList<QUrl>{item.url()});
-    d->m_newFileMenu->setViewShowsHiddenFiles(showHiddenFiles());
     d->m_newFileMenu->checkUpToDate();
 
     d->m_actionCollection->action(QStringLiteral("new"))->setEnabled(item.isDir());
