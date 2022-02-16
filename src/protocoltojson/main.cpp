576ee5c33aa8e3414b105e3e322a4ba95cf654c6
Tomas Janco <jancotomas@gmail.com>
2022-02-16 07:54:39 +0000

    Add missing "truncating" parameter https://bugs.kde.org/show_bug.cgi?id=450198

--- src/protocoltojson/main.cpp
+++ src/protocoltojson/main.cpp
@@ -78,6 +78,7 @@ int main(int argc, char *argv[])
                                       QStringLiteral("linking"),
                                       QStringLiteral("moving"),
                                       QStringLiteral("opening"),
+                                      QStringLiteral("truncating"),
                                       QStringLiteral("copyFromFile"),
                                       QStringLiteral("copyToFile"),
                                       QStringLiteral("renameFromFile"),
