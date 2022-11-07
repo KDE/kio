48cefcfa1e826707ef9aa8002dea6b0e6ce3a302
Friedrich W. H. Kossebau <kossebau@kde.org>
2022-11-07 17:54:01 +0100

    Emit deprecation warning for ioslave_defaults.h

--- src/core/ioslave_defaults.h
+++ src/core/ioslave_defaults.h
@@ -1,15 +1,16 @@
 /*
-    This file is part of the KDE libraries
-    SPDX-FileCopyrightText: 2001 Waldo Bastian <bastian@kde.org>
+    SPDX-FileCopyrightText: 2022 Friedrich W. H. Kossebau <kossebau@kde.org>
 
-    SPDX-License-Identifier: LGPL-2.0-only
+    SPDX-License-Identifier: LGPL-2.1-or-later
 */
 
-#ifndef _KIO_IOSLAVE_DEFAULTS_H
-#define _KIO_IOSLAVE_DEFAULTS_H
+#include <kiocore_export.h>
 
-// TODO KF6 drop this file
-
+#if KIOCORE_ENABLE_DEPRECATED_SINCE(5, 101)
 #include "ioworker_defaults.h"
-
+#if KIOCORE_DEPRECATED_WARNINGS_SINCE >= 0x056500
+#pragma message("Deprecated header. Since 5.101, use #include <kio/ioworker_defaults.h> instead")
+#endif
+#else
+#error "Include of deprecated header is disabled"
 #endif
