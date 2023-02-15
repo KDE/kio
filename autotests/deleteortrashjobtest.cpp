24dbe2d3598498d7de372fe087a04824ac03191b
Méven Car <meven@kde.org>
2023-02-15 16:43:58 +0100

    DeleteOrTrashJob: when trashing a file in trash:/ delete it instead
    BUG: 459545

--- autotests/deleteortrashjobtest.cpp
+++ autotests/deleteortrashjobtest.cpp
@@ -5,6 +5,7 @@
     SPDX-License-Identifier: LGPL-2.0-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
+#include "copyjob.h"
 #include "kiotesthelper.h"
 #include "mockcoredelegateextensions.h"
 
@@ -22,6 +23,7 @@ private Q_SLOTS:
     void deleteFileTest();
     void moveToTrashTest();
     void emptyTrashTest();
+    void deleteTrashFileTest();
 };
 
 QTEST_MAIN(DeleteOrTrashJobTest)
@@ -33,11 +35,9 @@ struct Info {
     MockAskUserInterface *askUserHandler = nullptr;
 };
 
-static Info createJob(AskIface::DeletionType deletionType)
+static Info createJobWithUrl(AskIface::DeletionType deletionType, const QUrl &url)
 {
-    const QString path = homeTmpDir() + "/delete_or_trash_job_test_file";
-    createTestFile(path);
-    auto *job = new KIO::DeleteOrTrashJob({QUrl::fromLocalFile(path)}, //
+    auto *job = new KIO::DeleteOrTrashJob({url}, //
                                           deletionType,
                                           AskIface::DefaultConfirmation,
                                           nullptr);
@@ -49,11 +49,19 @@ static Info createJob(AskIface::DeletionType deletionType)
     return {job, askUserHandler};
 }
 
+static Info createJob(AskIface::DeletionType deletionType)
+{
+    const QString path = homeTmpDir() + "delete_or_trash_job_test_file";
+    createTestFile(path);
+    return createJobWithUrl(deletionType, QUrl::fromLocalFile(path));
+}
+
 void DeleteOrTrashJobTest::deleteFileTest()
 {
     auto [job, askUserHandler] = createJob(AskIface::Delete);
     QVERIFY(job->exec());
     QCOMPARE(askUserHandler->m_askUserDeleteCalled, 1);
+    QCOMPARE(askUserHandler->m_delType, KIO::AskUserActionInterface::DeletionType::Delete);
 }
 
 void DeleteOrTrashJobTest::moveToTrashTest()
@@ -61,12 +69,32 @@ void DeleteOrTrashJobTest::moveToTrashTest()
     auto [job, askUserHandler] = createJob(AskIface::Trash);
     QVERIFY(job->exec());
     QCOMPARE(askUserHandler->m_askUserDeleteCalled, 1);
+    QCOMPARE(askUserHandler->m_delType, KIO::AskUserActionInterface::DeletionType::Trash);
 }
 
 void DeleteOrTrashJobTest::emptyTrashTest()
 {
     auto [job, askUserHandler] = createJob(AskIface::EmptyTrash);
     QVERIFY(job->exec());
+    QCOMPARE(askUserHandler->m_askUserDeleteCalled, 1);
+    QCOMPARE(askUserHandler->m_delType, KIO::AskUserActionInterface::DeletionType::EmptyTrash);
+}
+
+void DeleteOrTrashJobTest::deleteTrashFileTest()
+{
+    QStandardPaths::setTestModeEnabled(true);
+
+    const QString path = homeTmpDir() + "delete_or_trash_job_test_file";
+    createTestFile(path);
+
+    KIO::CopyJob *trashJob = KIO::move(QUrl::fromLocalFile(path), QUrl(QStringLiteral("trash:/")), KIO::HideProgressInfo);
+    trashJob->setUiDelegate(nullptr);
+    QVERIFY(trashJob->exec());
+
+    auto [job, askUserHandler] = createJobWithUrl(AskIface::Trash, QUrl("trash:/0-delete_or_trash_job_test_file"));
+    bool res = job->exec();
+    QVERIFY(res);
+    QCOMPARE(askUserHandler->m_delType, KIO::AskUserActionInterface::DeletionType::Delete);
     QCOMPARE(askUserHandler->m_askUserDeleteCalled, 1);
 }
 
