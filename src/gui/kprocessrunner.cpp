f8d3ba43b94bcbcc4b2ef796b2f93200d5882016
Ismael Asensio <isma.af@gmail.com>
2023-03-19 01:44:03 +0100

    Use SystemSettings versionless executable
    Instead of the deprecated versioned symlink which is removed
    in Plasma6 (https://phabricator.kde.org/T14763)

--- src/gui/kprocessrunner.cpp
+++ src/gui/kprocessrunner.cpp
@@ -309,7 +309,7 @@ void KProcessRunner::init(const KService::Ptr &service, const QString &serviceEn
         m_service = service;
         // Store the desktop name, used by debug output and for the systemd unit name
         m_desktopName = service->menuId();
-        if (m_desktopName.isEmpty() && m_executable == QLatin1String("systemsettings5")) {
+        if (m_desktopName.isEmpty() && m_executable == QLatin1String("systemsettings")) {
             m_desktopName = QStringLiteral("systemsettings.desktop");
         }
         if (m_desktopName.endsWith(QLatin1String(".desktop"))) { // always true, in theory
