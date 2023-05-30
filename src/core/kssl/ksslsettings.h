57cd8b9d3d46819543eadfcbbbc2ea392991ed96
Nicolas Fella <nicolas.fella@gmx.de>
2023-05-30 21:05:56 +0200

    Remove unused code from KSSLSettings

--- src/core/kssl/ksslsettings.h
+++ src/core/kssl/ksslsettings.h
@@ -50,99 +50,15 @@ public:
     bool warnOnEnter() const;
 
     /**
-     *  Change the user's warnOnEnter() setting
-     *  @param x true if the user is to be warned
-     *  @see warnOnEnter
-     */
-    void setWarnOnEnter(bool x);
-
-    /**
-     *  Does the user want to be warned on sending unencrypted data
-     *  @return true if the user wants to be warned
-     *  @see setWarnOnUnencrypted
-     */
-    bool warnOnUnencrypted() const;
-
-    /**
-     *  Change the user's warnOnUnencrypted() setting
-     *  @param x true if the user is to be warned
-     *  @see warnOnUnencrypted
-     */
-    void setWarnOnUnencrypted(bool x);
-
-    /**
      *  Does the user want to be warned on leaving SSL mode
      *  @return true if the user wants to be warned
      */
     bool warnOnLeave() const;
 
     /**
-     *  Change the user's warnOnLeave() setting
-     *  @param x true if the user is to be warned
-     *  @see warnOnLeave
-     */
-    void setWarnOnLeave(bool x);
-
-    /**
-     *  Does the user want to be warned during mixed SSL/non-SSL mode
-     *  @return true if the user wants to be warned
-     */
-    bool warnOnMixed() const;
-
-    /**
-     *  Does the user want to use the Entropy Gathering Daemon?
-     *  @return true if the user wants to use EGD
-     */
-    bool useEGD() const;
-
-    /**
-     *  Does the user want to use an entropy file?
-     *  @return true if the user wants to use an entropy file
-     */
-    bool useEFile() const;
-
-    /**
-     *  Does the user want X.509 client certificates to always be sent when
-     *  possible?
-     *  @return true if the user always wants a certificate sent
-     */
-    bool autoSendX509() const;
-
-    /**
-     *  Does the user want to be prompted to send X.509 client certificates
-     *  when possible?
-     *  @return true if the user wants to be prompted
-     */
-    bool promptSendX509() const;
-
-    /**
-     *  Get the OpenSSL cipher list for selecting the list of ciphers to
-     *  use in a connection.
-     *  @return the cipher list
-     */
-    QString getCipherList();
-
-    /**
-     *  Get the configured path to the entropy gathering daemon or entropy
-     *  file.
-     *  @return the path
-     */
-    QString &getEGDPath();
-
-    /**
      *  Load the user's settings.
      */
     void load();
-
-    /**
-     *  Revert to default settings.
-     */
-    void defaults();
-
-    /**
-     *  Save the current settings.
-     */
-    void save();
 
 private:
     std::unique_ptr<KSSLSettingsPrivate> const d;
