aae2e2b47c239d03c84f7a8873baa2271af9da21
Oliver Beard <olib141@outlook.com>
2023-06-10 09:48:08 +0100

    filewidgets/kfilewidget: Select and focus filename after canceling overwrite
    This ensures that the filename can be typed immediately after pressing the save button and cancelling an overwrite.
    BUG: 444515
    
    
    (cherry picked from commit cff4ec757e618478bdcb5f07a3e7ea6f5da9f3c6)

--- src/filewidgets/kfilewidget.cpp
+++ src/filewidgets/kfilewidget.cpp
@@ -1746,6 +1746,9 @@ bool KFileWidgetPrivate::toOverwrite(const QUrl &url)
                                                      KMessageBox::Notify | KMessageBox::Dangerous);
 
         if (ret != KMessageBox::Continue) {
+            m_locationEdit->setFocus();
+            setNonExtSelection();
+
             return false;
         }
         return true;
