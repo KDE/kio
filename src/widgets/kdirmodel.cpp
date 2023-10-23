f69a7b2a305737a1d20c217b2a56a0c3436f3354
Antoine Gonzalez <antoine.gonzalez@enioka.com>
2023-10-23 16:29:07 +0200

    KDirModel: Limit details fetching for network fs
    Prevent loading extra details when on a network (slow) fs, not detected
    by QUrl::isLocalFile.

--- src/widgets/kdirmodel.cpp
+++ src/widgets/kdirmodel.cpp
@@ -1229,7 +1229,7 @@ bool KDirModel::hasChildren(const QModelIndex &parent) const
     if (static_cast<const KDirModelDirNode *>(parentNode)->isPopulated()) {
         return !static_cast<const KDirModelDirNode *>(parentNode)->m_childNodes.isEmpty();
     }
-    if (parentItem.isLocalFile()) {
+    if (parentItem.isLocalFile() && !parentItem.isSlow()) {
         QDir::Filters filters = QDir::Dirs | QDir::NoDotAndDotDot;
 
         if (d->m_dirLister->dirOnlyMode()) {
@@ -1308,7 +1308,8 @@ bool KDirModel::canFetchMore(const QModelIndex &parent) const
 
     KDirModelNode *node = static_cast<KDirModelNode *>(parent.internalPointer());
     const KFileItem &item = node->item();
-    return item.isDir() && !static_cast<KDirModelDirNode *>(node)->isPopulated() && static_cast<KDirModelDirNode *>(node)->m_childNodes.isEmpty();
+    return item.isDir() && !item.isSlow() && !static_cast<KDirModelDirNode *>(node)->isPopulated()
+        && static_cast<KDirModelDirNode *>(node)->m_childNodes.isEmpty();
 }
 
 void KDirModel::fetchMore(const QModelIndex &parent)
