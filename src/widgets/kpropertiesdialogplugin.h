b9d842b9506f90456b1f26c3edfcab5c9722ac78
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-11-26 09:44:42 +0100

    Add KF5 include for KPropertiesDialogPlugin
    It will be separate in KF6, this will make life for consumers a bit easier

---
+++ src/widgets/kpropertiesdialogplugin.h
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: 2023 Alexander Lohnau <alexander.lohnau@gmx.de>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+// In KF6, the plugin is a separate header
+#include <kpropertiesdialog.h>
