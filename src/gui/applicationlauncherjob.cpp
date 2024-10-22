e61b67dab37ad048a3923f5c49b33b1cb0167c44
Jin Liu <m.liu.jin@gmail.com>
2024-10-22 05:40:33 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Méven Car <meven@kde.org>

--- src/gui/applicationlauncherjob.cpp
+++ src/gui/applicationlauncherjob.cpp
@@ -127,7 +127,7 @@ void KIO::ApplicationLauncherJob::start()
 {
     if (d->m_urls.size() == 1) {
         const QUrl url = d->m_urls.first();
-        if (url.isLocalFile() && !QFileInfo(url.toLocalFile()).exists()) {
+        if (url.isLocalFile() && !QFile::exists(url.toLocalFile())) {
             // Don't launch an application for a non-existing file (e.g. a broken symlink)
             setError(KIO::ERR_DOES_NOT_EXIST);
             setErrorText(url.toDisplayString());
