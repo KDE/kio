KDE/kio

commit 35e575eb7d457c6e972ca373e2c408c31e4a7358

kmountpoint: Handle device IDs for bind mounts
BUG: 494129

src/core/kmountpoint.cpp
@@ -359,7 +359,15 @@ KMountPoint::List KMountPoint::currentMountPoints(DetailsNeededFlags infoNeeded)
                 mp->d->m_mountPoint = QFile::decodeName(mnt_fs_get_target(fs));
                 mp->d->m_mountType = QFile::decodeName(mnt_fs_get_fstype(fs));
                 mp->d->m_isNetFs = mnt_fs_is_netfs(fs) == 1;
-                mp->d->m_deviceId = mnt_fs_get_devno(fs);
+
+                // handle bind mounts
+                if (mp->d->m_mountedFrom != mp->d->m_mountPoint) {
+                    if (QT_STATBUF buff; QT_LSTAT(mp->d->m_mountPoint.toLatin1().constData(), &buff) == 0) {
+                        mp->d->m_deviceId = buff.st_dev;
+                    }
+                } else {
+                    mp->d->m_deviceId = mnt_fs_get_devno(fs);
+                }
 
                 if (infoNeeded & NeedMountOptions) {
                     mp->d->m_mountOptions = QFile::decodeName(mnt_fs_get_options(fs)).split(QLatin1Char(','));

commit abb8c408fbb02955d35653958e3a5b25c65c6a8f

KMountPoint: use mount id to match filesystem if possible
This should work with kernel 5.8+. This is the supposed way to match the
actual mount from /proc/self/mountinfo with the actual file. Not doing
it this way something like

747 253 0:79 / /media/smb/hostname/share rw,relatime shared:861 - autofs /etc/auto.master.d/auto.smb rw,fd=10,pgrp=1109,timeout=15,minproto=5,maxproto=5,offset,pipe_ino=11848
983 747 0:82 / /media/smb/hostname/share rw,nosuid,nodev,noexec,relatime shared:998 - cifs //hostname/share rw,vers=3.1.1,sec=krb5..

will show autofs, mounted from /etc.. in dolphin instead of the proper cifs and mounted from //hostname...

This was actually working some time ago, but commit id

35e575eb7d457c6e972ca373e2c408c31e4a7358

broke this since it uses the stat-ed device id for the fs entry insted the one from column 3
and thus it replaced the autofs dev id to 0:82 in the case above and made this the acutal mount insted of
the one below. This makes findByPath using the wrong actual "line".

There are reasons for 35e575eb7d457c6e972ca373e2c408c31e4a7358,
espescially btrfs and probaby bind mounts, but we might try as hard as
we want: the correct answer is mount id from statx, everything else is
guessing

src/core/ConfigureChecks.cmake
@@ -37,6 +37,21 @@ if (LIBC_IS_GLIBC)
             return 0;
         }
     " HAVE_STATX)
+    check_cxx_source_compiles(
+        "#include <sys/types.h>
+        #include <sys/stat.h>
+        #include <unistd.h>
+        #include <fcntl.h>
+
+        int main()
+        {
+            struct statx st;
+            int res = statx(AT_FDCWD, \".\", AT_NO_AUTOMOUNT, STATX_MNT_ID, &st);
+            st.stx_mnt_id = 1;
+        }"  HAVE_STATX_MNT_ID)
+
 else()
     set(HAVE_STATX 0)
+    set(HAVE_STATX_MNT_ID 0)
 endif()
+

src/core/config-kmountpoint.h.cmake
@@ -8,3 +8,5 @@
 #cmakedefine01 HAVE_SYS_PARAM_H
 
 #cmakedefine01 HAVE_VOLMGT
+
+#cmakedefine01 HAVE_STATX_MNT_ID

src/core/kmountpoint.cpp
@@ -48,6 +48,10 @@ static const Qt::CaseSensitivity cs = Qt::CaseSensitive;
 // Linux
 #if HAVE_LIB_MOUNT
 #include <libmount/libmount.h>
+#if HAVE_STATX_MNT_ID
+#include <fcntl.h>
+#include <sys/stat.h>
+#endif
 #endif
 
 static bool isNetfs(const QString &mountType)
@@ -85,6 +89,10 @@ public:
     QString m_mountType;
     QStringList m_mountOptions;
     dev_t m_deviceId = 0;
+#if HAVE_STATX_MNT_ID
+    // the mount id seems to start at 1. 0 should be safe as a undefined default
+    __u64 m_mountId = 0;
+#endif
     bool m_isNetFs = false;
 };
 
@@ -359,6 +367,13 @@ KMountPoint::List KMountPoint::currentMountPoints(DetailsNeededFlags infoNeeded)
                 mp->d->m_mountPoint = QFile::decodeName(mnt_fs_get_target(fs));
                 mp->d->m_mountType = QFile::decodeName(mnt_fs_get_fstype(fs));
                 mp->d->m_isNetFs = mnt_fs_is_netfs(fs) == 1;
+#if HAVE_STATX_MNT_ID
+                // mnt_fs_get_id actually returns a int whereas the kernel uses __u64
+                int mnt_id = mnt_fs_get_id(fs);
+                if (mnt_id != -1) {
+                    mp->d->m_mountId = (__u64)mnt_id;
+                }
+#endif
 
                 // handle bind mounts
                 if (mp->d->m_mountedFrom != mp->d->m_mountPoint) {
@@ -447,6 +462,19 @@ KMountPoint::Ptr KMountPoint::List::findByPath(const QString &path) const
 #endif
 
     KMountPoint::Ptr result;
+#if HAVE_STATX_MNT_ID
+    // If we have statx, there is no need to guess. take the mount id from statx and get the mountpoint.
+    if (struct statx buff; statx(0, QFile::encodeName(realPath).constData(), AT_NO_AUTOMOUNT, STATX_MNT_ID, &buff) == 0) {
+        auto it = std::find_if(this->cbegin(), this->cend(), [&buff](const KMountPoint::Ptr &mountPtr) {
+            return mountPtr->d->m_mountId == buff.stx_mnt_id;
+        });
+
+        if (it != this->cend()) {
+            return *it;
+        }
+    }
+#endif
+
 
     if (QT_STATBUF buff; QT_LSTAT(QFile::encodeName(realPath).constData(), &buff) == 0) {
         auto it = std::find_if(this->cbegin(), this->cend(), [&buff, &realPath](const KMountPoint::Ptr &mountPtr) {