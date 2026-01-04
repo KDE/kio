KDE/kio

commit b719ce072b61fefa40cd035487c981e1ededd573

KPropertiesDialog: Use KFileItem::timeString
Instead of formatting it manually.

src/widgets/kpropertiesdialogbuiltin_p.cpp
@@ -482,23 +482,22 @@ KFilePropsPlugin::KFilePropsPlugin(KPropertiesDialog *_props)
 
     // Time widgets
     if (!d->bMultiple) {
-        QLocale locale;
-        if (const QDateTime dt = firstItem.time(KFileItem::CreationTime); !dt.isNull()) {
-            d->m_ui->createdTimeLabel->setText(locale.toString(dt, QLocale::LongFormat));
+        if (const QString timeString = firstItem.timeString(KFileItem::CreationTime); !timeString.isEmpty()) {
+            d->m_ui->createdTimeLabel->setText(timeString);
         } else {
             d->m_ui->createdTimeLabel->hide();
             d->m_ui->createdTimeLabel_Left->hide();
         }
 
-        if (const QDateTime dt = firstItem.time(KFileItem::ModificationTime); !dt.isNull()) {
-            d->m_ui->modifiedTimeLabel->setText(locale.toString(dt, QLocale::LongFormat));
+        if (const QString timeString = firstItem.timeString(KFileItem::ModificationTime); !timeString.isEmpty()) {
+            d->m_ui->modifiedTimeLabel->setText(timeString);
         } else {
             d->m_ui->modifiedTimeLabel->hide();
             d->m_ui->modifiedTimeLabel_Left->hide();
         }
 
-        if (const QDateTime dt = firstItem.time(KFileItem::AccessTime); !dt.isNull() && hasAccessTime) {
-            d->m_ui->accessTimeLabel->setText(locale.toString(dt, QLocale::LongFormat));
+        if (const QString timeString = firstItem.timeString(KFileItem::AccessTime); !timeString.isEmpty()) {
+            d->m_ui->accessTimeLabel->setText(timeString);
         } else {
             d->m_ui->accessTimeLabel->hide();
             d->m_ui->accessTimeLabel_Left->hide();

commit a017f227d7dcdd8623c92f7994a1e764b023d1ae

KPropertiesDialog: Hide access time when fs has noatime or nodiratime
Reintroduce the feature added in a9c40a7dfb7db5b2caa80b62e4234f44cb55bbcd
That regressed in b719ce072b61fefa40cd035487c981e1ededd573.

src/widgets/kpropertiesdialogbuiltin_p.cpp
@@ -496,7 +496,7 @@ KFilePropsPlugin::KFilePropsPlugin(KPropertiesDialog *_props)
             d->m_ui->modifiedTimeLabel_Left->hide();
         }
 
-        if (const QString timeString = firstItem.timeString(KFileItem::AccessTime); !timeString.isEmpty()) {
+        if (const QString timeString = firstItem.timeString(KFileItem::AccessTime); hasAccessTime && !timeString.isEmpty()) {
             d->m_ui->accessTimeLabel->setText(timeString);
         } else {
             d->m_ui->accessTimeLabel->hide();