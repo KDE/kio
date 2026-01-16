KDE/kio

commit 35e575eb7d457c6e972ca373e2c408c31e4a7358

kmountpoint: Handle device IDs for bind mounts
BUG: 494129

src/core/kmountpoint.cpp
@@ -359,7 +359,15 @@ KMountPoint::List KMountPoint::currentMountPoints(DetailsNeededFlags infoNeeded)
                 mp->d->m_mountPoint = QFile::decodeName(mnt_fs_get_target(fs));
                 mp->d->m_mountType = QFile::decodeName(mnt_fs_get_fstype(fs));
                 mp->d->m_isNetFs = mnt_fs_is_netfs(fs) == 1;
-                mp->d->m_deviceId = mnt_fs_get_devno(fs);
+
+                // handle bind mounts
+                if (mp->d->m_mountedFrom != mp->d->m_mountPoint) {
+                    if (QT_STATBUF buff; QT_LSTAT(mp->d->m_mountPoint.toLatin1().constData(), &buff) == 0) {
+                        mp->d->m_deviceId = buff.st_dev;
+                    }
+                } else {
+                    mp->d->m_deviceId = mnt_fs_get_devno(fs);
+                }
 
                 if (infoNeeded & NeedMountOptions) {
                     mp->d->m_mountOptions = QFile::decodeName(mnt_fs_get_options(fs)).split(QLatin1Char(','));

commit 4709688aa80d7fc2cac7bce7285f6830e4cbcf21

kmountpoint: use statx to get device id
35e575eb7d has introduced stat to fix issues with bind mounts. However,
using stat on every mountpoint could lead to serius performance issues
when it comes to network mounts.

We cannot use libmount's mnt_fs_get_devno as the id is different than
what would result from stating a file on that mount.

Instead, we use statx which allows us to ask the kernel for only a
cached value, which is enough for us, because the kernel determines
the device id. This saves us a network roundtrip and avoids blocking.

autotests/kmountpointtest.cpp
@@ -80,6 +80,8 @@ void KMountPointTest::testCurrentMountPoints()
         } else {
             QCOMPARE(homeMountPoint->mountPoint(), QDir(QStringLiteral("/home")).canonicalPath());
         }
+
+        QCOMPARE(homeMountPoint->deviceId(), homeStatBuff.st_dev);
     } else {
         qDebug() << "/home doesn't seem to exist, skipping test";
     }

src/core/kmountpoint.cpp
@@ -48,6 +48,7 @@ static const Qt::CaseSensitivity cs = Qt::CaseSensitive;
 // Linux
 #if HAVE_LIB_MOUNT
 #include <libmount/libmount.h>
+#include <sys/sysmacros.h>
 #if HAVE_STATX_MNT_ID
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -371,13 +372,8 @@ KMountPoint::List KMountPoint::currentMountPoints(DetailsNeededFlags infoNeeded)
                 }
 #endif
 
-                // handle bind mounts
-                if (mp->d->m_mountedFrom != mp->d->m_mountPoint) {
-                    if (QT_STATBUF buff; QT_LSTAT(mnt_fs_get_target(fs), &buff) == 0) {
-                        mp->d->m_deviceId = buff.st_dev;
-                    }
-                } else {
-                    mp->d->m_deviceId = mnt_fs_get_devno(fs);
+                if (struct statx buff; statx(AT_FDCWD, mnt_fs_get_target(fs), AT_STATX_DONT_SYNC, STATX_INO, &buff) == 0) {
+                    mp->d->m_deviceId = makedev(buff.stx_dev_major, buff.stx_dev_minor);
                 }
 
                 if (infoNeeded & NeedMountOptions) {