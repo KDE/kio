ae822339928fbf0c58c85cb295edb896a8d36a99
Méven Car <meven@kde.org>
2026-06-13 13:30:21 +0200

    filepreviewjobtest: Correct email in SPDX header

--- autotests/filepreviewjobtest.h
+++ autotests/filepreviewjobtest.h
@@ -1,6 +1,6 @@
 /*
     This file is part of the KDE libraries
-    SPDX-FileCopyrightText: 2026 Meven Car <meven.car@collabora.com>
+    SPDX-FileCopyrightText: 2026 Meven Car <meven@kde.org>
 
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
